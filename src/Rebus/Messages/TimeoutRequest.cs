a28a63279f6c1de8eda5b0fbeb0d5cbbb5638cb9
Mogens Heller Grabe <mookid8000@gmail.com>
2012-01-24 11:04:20 +0100

    added a couple of comments

--- src/Rebus/Messages/TimeoutRequest.cs
+++ src/Rebus/Messages/TimeoutRequest.cs
@@ -2,10 +2,22 @@ using System;
 
 namespace Rebus.Messages
 {
+    /// <summary>
+    /// Requests a delayed reply from the Timeout Service. Upon receiving
+    /// this message, the Timeout Service will calculate the UTC time of when the timeout
+    /// should expire, wait, and then reply with a <see cref="TimeoutReply"/>.,
+    /// </summary>
     public class TimeoutRequest : IRebusControlMessage
     {
+        /// <summary>
+        /// For how long should the reply be delayed?
+        /// </summary>
         public TimeSpan Timeout { get; set; }
 
+        /// <summary>
+        /// Allows for specifying a correlation ID that the Timeout Service will
+        /// return with the <see cref="TimeoutReply"/>.
+        /// </summary>
         public string CorrelationId { get; set; }
     }
 }
