0186b7dede2c89b06da74abaa03c3ccd4da9fe86
Mogens Heller Grabe <mookid8000@gmail.com>
2012-01-25 10:05:29 +0100

    commented some stuff

--- src/Rebus/Configuration/ConfigurationException.cs
+++ src/Rebus/Configuration/ConfigurationException.cs
@@ -2,8 +2,14 @@
 
 namespace Rebus.Configuration
 {
+    /// <summary>
+    /// Exception that should be thrown in the event that a given configuration is somehow invalid.
+    /// </summary>
     public class ConfigurationException : ApplicationException
     {
+        /// <summary>
+        /// Constructs the exception with the specified message.
+        /// </summary>
         public ConfigurationException(string message, params object[] objs)
             : base(string.Format(message, objs))
         {
--- src/Rebus/Messages/TimeoutReply.cs
+++ src/Rebus/Messages/TimeoutReply.cs
@@ -2,10 +2,20 @@ using System;
 
 namespace Rebus.Messages
 {
+    /// <summary>
+    /// This is the reply that the Timeout Service will send back to the
+    /// timeout requestor upon completion of the timeout.
+    /// </summary>
     public class TimeoutReply : IRebusControlMessage
     {
+        /// <summary>
+        /// The UTC time of when the timeout expired.
+        /// </summary>
         public DateTime DueTime { get; set; }
 
+        /// <summary>
+        /// The correlation ID as specified in the <see cref="TimeoutRequest"/>.
+        /// </summary>
         public string CorrelationId { get; set; }
     }
 }
