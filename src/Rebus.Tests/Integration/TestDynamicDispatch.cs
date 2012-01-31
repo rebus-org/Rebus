5c6627e2c6247ab736932ddfc9c581036b021d85
Mogens Heller Grabe <mookid8000@gmail.com>
2012-01-31 22:29:20 +0100

    added test for dynamic dispatch

---
+++ src/Rebus.Tests/Integration/TestDynamicDispatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Rebus.Tests.Integration
+{
+    [TestFixture, Category(TestCategories.Integration)]
+    public class TestDynamicDispatch : RebusBusMsmqIntegrationTestBase
+    {
+        [Test, Ignore("not sure if this one should pass... ?")]
+        public void HandlerWithDynamicGetAllCalls()
+        {
+            const string receiverInputQueueName = "test.dynamic.receiver";
+            var sender = CreateBus("test.dynamic.sender", new HandlerActivatorForTesting()).Start(1);
+
+            var dynamicHandler = new DynamicHandler();
+            CreateBus(receiverInputQueueName, new HandlerActivatorForTesting()
+                                                  .UseHandler(dynamicHandler)).Start(1);
+
+            sender.Send(receiverInputQueueName, new JustSomeMessage{Greeting = "Hello world!"});
+
+            Thread.Sleep(500);
+
+            dynamicHandler.GotIt.ShouldBe(true);
+        }
+
+        class JustSomeMessage
+        {
+            public string Greeting { get; set; }
+        }
+    }
+
+    public class DynamicHandler : IHandleMessages<object>
+    {
+        public bool GotIt { get; set; }
+
+        public void Handle(dynamic message)
+        {
+            Console.WriteLine(message.GetType());
+
+            Console.WriteLine(string.Join(Environment.NewLine, message.GetType().GetType()));
+
+            if (message.Greeting == "Hello world!")
+            {
+                GotIt = true;
+            }
+        }
+    }
+}
