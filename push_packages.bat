d552264845003105cae7ccbd40435d636d30354b
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-03-15 23:06:51 +0100

    Added version support to push_packages.bat.

--- push_packages.bat
+++ push_packages.bat
@@ -1,1 +1,10 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe scripts\build.proj /t:build;pushNugetPackages
+@echo off
+if [%1] == [] goto NO_VERSION
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe scripts\build.proj /t:build;pushNugetPackages /p:Version=%1
+goto DONE
+
+:NO_VERSION
+echo Syntax: push_packages ^<version^>
+
+:DONE
