7201148abd4503ccea8f7ef5fd49ff41e355affd
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-03-15 23:11:28 +0100

    Added verbosity minimal to msbuild bat files.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe scripts\build.proj /p:Version=%1
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe scripts\build.proj /v:m /p:Version=%1
--- push_packages.bat
+++ push_packages.bat
@@ -1,7 +1,7 @@
 @echo off
 if [%1] == [] goto NO_VERSION
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe scripts\build.proj /t:build;pushNugetPackages /p:Version=%1
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe scripts\build.proj /v:m /t:build;pushNugetPackages /p:Version=%1
 goto DONE
 
 :NO_VERSION
