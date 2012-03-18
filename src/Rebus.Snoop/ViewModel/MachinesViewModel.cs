652a762d75b820b5b4dfe3eb0359bd21c1799c7f
Mogens Heller Grabe <mookid8000@gmail.com>
2012-03-18 16:09:10 +0100

    started building Snoop

---
+++ src/Rebus.Snoop/ViewModel/MachinesViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Rebus.Snoop.Machines
+{
+    public class MachinesViewModel : ViewModel
+    {
+        readonly ObservableCollection<Machine> machines = new ObservableCollection<Machine>();
+
+        public MachinesViewModel()
+        {
+            //if (IsInDesignMode)
+            {
+                machines.Add(new Machine
+                                 {
+                                     MachineName = "\\some_machine",
+                                     Queues =
+                                         {
+                                             new Queue {QueueName = "someService.input", Messages={new Message{MessageHeader="msg1"}, new Message{MessageHeader="msg2"}, new Message{MessageHeader="msg3"}}},
+                                             new Queue {QueueName = "someService.error", Messages={new Message{MessageHeader="some.error.msg"},}},
+                                             new Queue {QueueName = "anotherService.input"},
+                                             new Queue {QueueName = "anotherService.error"},
+                                         }
+                                 });
+                machines.Add(new Machine
+                                 {
+                                     MachineName = "\\another_machine",
+                                     Queues =
+                                         {
+                                             new Queue {QueueName = "aService.input", Messages={new Message{MessageHeader="msg1"}, new Message{MessageHeader="msg2"}, new Message{MessageHeader="msg3"}}},
+                                             new Queue {QueueName = "aService.error"},
+                                             new Queue {QueueName = "unrelated"},
+                                             new Queue {QueueName = "another.unrelated"},
+                                             new Queue {QueueName = "yet.another.unrelated",Messages=
+                                                                                                {
+                                                                                                    new Message{MessageHeader="msg1", Bytes=12}, 
+                                                                                                    new Message{MessageHeader="msg2", Bytes=90}, 
+                                                                                                    new Message{MessageHeader="msg3", Bytes=1024},
+                                                                                                    new Message{MessageHeader="msg4", Bytes=2048}, 
+                                                                                                    new Message{MessageHeader="msg5", Bytes=10249090}, 
+                                                                                                    new Message{MessageHeader="msg6", Bytes=3424234},
+                                                                                                    new Message{MessageHeader="msg7", Bytes=15325323}, 
+                                                                                                    new Message{MessageHeader="msg8", Bytes=15352}, 
+                                                                                                    new Message{MessageHeader="msg9", Bytes=12},
+                                                                                                }},
+                                         }
+                                 });
+                machines.Add(new Machine {MachineName = "\\yet_another_machine"});
+            }
+        }
+
+        public ObservableCollection<Machine> Machines
+        {
+            get { return machines; }
+        }
+    }
+
+    public class Machine : ViewModel
+    {
+        readonly ObservableCollection<Queue> queues = new ObservableCollection<Queue>();
+        string machineName;
+
+        public string MachineName
+        {
+            get { return machineName; }
+            set { SetValue("MachineName", value); }
+        }
+
+        public ObservableCollection<Queue> Queues
+        {
+            get { return queues; }
+        }
+    }
+
+    public class Queue : ViewModel
+    {
+        readonly ObservableCollection<Message> messages = new ObservableCollection<Message>();
+        string queueName;
+
+        public string QueueName
+        {
+            get { return queueName; }
+            set { SetValue("QueueName", value); }
+        }
+
+        public ObservableCollection<Message> Messages
+        {
+            get { return messages; }
+        }
+    }
+
+    public class Message : ViewModel
+    {
+        string messageHeader;
+
+        public string MessageHeader
+        {
+            get { return messageHeader; }
+            set { SetValue("MessageHeader", value); }
+        }
+
+        int bytes;
+
+        public int Bytes
+        {
+            get { return bytes; }
+            set { SetValue("Bytes", value); }
+        }
+
+        
+    }
+}
---
+++ src/Rebus.Snoop/ViewModel/ViewModel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+using GalaSoft.MvvmLight;
+
+namespace Rebus.Snoop.Machines
+{
+    public abstract class ViewModel : ViewModelBase
+    {
+        protected void SetValue(string propertyName, object value)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException("You need to specify the name of a property.", "propertyName");
+            }
+
+            if (char.IsLower(propertyName[0]))
+            {
+                throw new ArgumentException(string.Format("{0} is not a valid property name - property names must start with an upper case letter!", propertyName), "propertyName");
+            }
+
+            var fieldName = char.ToLower(propertyName[0]) + propertyName.Substring(1);
+
+            var fieldInfo = GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (fieldInfo == null)
+            {
+                throw new InvalidOperationException(
+                    string.Format("Attempted to set value of {0} to {1}, but the matching field named {2} could not be found!",
+                        propertyName, value, fieldName));
+            }
+
+            var oldValue = fieldInfo.GetValue(this);
+
+            if (ReferenceEquals(null, oldValue) && ReferenceEquals(null, value)) return;
+            if (!ReferenceEquals(null, oldValue) && !ReferenceEquals(null, value) && oldValue.Equals(value)) return;
+
+            fieldInfo.SetValue(this, value);
+
+            RaisePropertyChanged(propertyName, oldValue, value, true);
+        }    
+    }
+}
