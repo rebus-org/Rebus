775c3dc38ffaa3f1f99389d9829ececc9917d889
Mogens Heller Grabe <mookid8000@gmail.com>
2012-05-08 22:39:39 +0200

    added fluent MongoDB configuration thingies

---
+++ src/Rebus.MongoDb/MongoDbExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Rebus.Configuration.Configurers;
+
+namespace Rebus.MongoDb
+{
+    public static class MongoDbExtensions
+    {
+        public static void StoreInMongoDb(this SagaConfigurer configurer, string connectionString, string collectionName)
+        {
+            configurer.Use(new MongoDbSagaPersister(connectionString, collectionName));
+        }
+
+        public static void StoreInMongoDb(this SubscriptionsConfigurer configurer, string connectionString, string collectionName)
+        {
+            configurer.Use(new MongoDbSubscriptionStorage(connectionString, collectionName));
+        }
+    }
+}
--- src/Rebus/Persistence/SqlServer/SqlServerExtensions.cs
+++ src/Rebus/Persistence/SqlServer/SqlServerExtensions.cs
@@ -4,14 +4,14 @@ namespace Rebus.Persistence.SqlServer
 {
     public static class SqlServerExtensions
     {
-         public static void StoreInSqlServer(this SagaConfigurer configurer, string connectionString, string sagaTableName, string sagaIndexTableName)
-         {
-             configurer.Use(new SqlServerSagaPersister(connectionString, sagaIndexTableName, sagaTableName));
-         }
+        public static void StoreInSqlServer(this SagaConfigurer configurer, string connectionString, string sagaTableName, string sagaIndexTableName)
+        {
+            configurer.Use(new SqlServerSagaPersister(connectionString, sagaIndexTableName, sagaTableName));
+        }
 
         public static void StoreInSqlServer(this SubscriptionsConfigurer configurer, string connectionString, string subscriptionsTableName)
-         {
-             configurer.Use(new SqlServerSubscriptionStorage(connectionString, subscriptionsTableName));
-         }
+        {
+            configurer.Use(new SqlServerSubscriptionStorage(connectionString, subscriptionsTableName));
+        }
     }
 }
