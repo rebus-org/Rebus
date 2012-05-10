d08cb4e7b44a12c57fe949993c8ee3c01895b698
Mogens Heller Grabe <mookid8000@gmail.com>
2012-05-10 22:31:33 +0200

    added very simple handler activator to allow for simple, non-DI-enabled handler activation

---
+++ src/Rebus.Tests/Unit/TestSimpleHandlerActivator.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using Shouldly;
+
+namespace Rebus.Tests.Unit
+{
+    [TestFixture]
+    public class TestSimpleHandlerActivator : FixtureBase
+    {
+        SimpleHandlerActivator activator;
+
+        protected override void DoSetUp()
+        {
+            activator = new SimpleHandlerActivator();
+        }
+
+        [Test]
+        public void YieldsNoHandlersWhenNothingHasBeenRegistered()
+        {
+            // arrange
+                        
+
+            // act
+            var instances = activator.GetHandlerInstancesFor<string>();
+
+            // assert
+            instances.Count().ShouldBe(0);
+        }
+
+        [Test]
+        public void CanCreateHandlerFromRegisteredType()
+        {
+            // arrange
+            activator.Register(typeof (SomeHandler));
+
+            // act
+            var instances = activator.GetHandlerInstancesFor<string>().ToList();
+
+            // assert
+            instances.Count.ShouldBe(1);
+            instances[0].ShouldBeTypeOf<SomeHandler>();
+        }
+
+        [Test]
+        public void WillHappilyCreateMultipleInstances()
+        {
+            // arrange
+            activator.Register(typeof (SomeHandler))
+                .Register(typeof (AnotherHandler))
+                .Register(typeof (ThirdHandler));
+
+            // act
+            var stringHandlers = activator.GetHandlerInstancesFor<string>().ToList();
+            var dateTimeHandlers = activator.GetHandlerInstancesFor<DateTime>().ToList();
+
+            // assert
+            stringHandlers.Count.ShouldBe(3);
+            stringHandlers[0].ShouldBeTypeOf<SomeHandler>();
+            stringHandlers[1].ShouldBeTypeOf<AnotherHandler>();
+            stringHandlers[2].ShouldBeTypeOf<ThirdHandler>();
+
+            dateTimeHandlers.Count.ShouldBe(1);
+            dateTimeHandlers[0].ShouldBeTypeOf<ThirdHandler>();
+        }
+
+        [Test]
+        public void WillCreateHandlerFromSuppliesFactoryMethodAsWell()
+        {
+            // arrange
+            activator.Register(() => new ThirdHandler());
+
+            // act
+            var stringHandlers = activator.GetHandlerInstancesFor<string>().ToList();
+            var dateTimeHandlers = activator.GetHandlerInstancesFor<DateTime>().ToList();
+
+            // assert
+            stringHandlers.Count.ShouldBe(1);
+            stringHandlers[0].ShouldBeTypeOf<ThirdHandler>();
+
+            dateTimeHandlers.Count.ShouldBe(1);
+            dateTimeHandlers[0].ShouldBeTypeOf<ThirdHandler>();
+        }
+
+        class ThirdHandler : IHandleMessages<string>, IHandleMessages<DateTime>
+        {
+            public void Handle(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Handle(DateTime message)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class AnotherHandler : IHandleMessages<string>
+        {
+            public void Handle(string message)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        class SomeHandler : IHandleMessages<string>
+        {
+            public void Handle(string message)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/Rebus/SimpleHandlerActivator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rebus
+{
+    /// <summary>
+    /// Very simple implementation of the handler activator that allows a bunch of types to be manually registered,
+    /// either with their type or with a factory method.
+    /// </summary>
+    public class SimpleHandlerActivator : IActivateHandlers
+    {
+        readonly Dictionary<Type, List<Func<object>>> activators = new Dictionary<Type, List<Func<object>>>();
+
+        public SimpleHandlerActivator Register(Type handlerType)
+        {
+            InnerRegister(handlerType, () => Activator.CreateInstance(handlerType));
+
+            return this;
+        }
+
+        public SimpleHandlerActivator Register<THandler>(Func<THandler> handlerFactory)
+        {
+            InnerRegister(typeof(THandler), () => handlerFactory());
+
+            return this;
+        }
+
+        void InnerRegister(Type handlerType, Func<object> factory)
+        {
+            var handlerInterfaces = handlerType.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof (IHandleMessages<>));
+
+            foreach (var handlerInterface in handlerInterfaces)
+            {
+                if (!activators.ContainsKey(handlerInterface))
+                    activators[handlerInterface] = new List<Func<object>>();
+
+                activators[handlerInterface].Add(factory);
+            }
+        }
+
+        public IEnumerable<IHandleMessages<T>> GetHandlerInstancesFor<T>()
+        {
+            if (!activators.ContainsKey(typeof(IHandleMessages<T>)))
+                return new IHandleMessages<T>[0];
+
+            return activators[typeof(IHandleMessages<T>)]
+                .Select(f => f()).Cast<IHandleMessages<T>>()
+                .ToArray();
+        }
+
+        public void Release(IEnumerable handlerInstances)
+        {
+        }
+    }
+}
