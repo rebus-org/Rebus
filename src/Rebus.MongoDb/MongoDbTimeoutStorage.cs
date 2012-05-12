52e61059cecf984654574a384fbc1394fd34d95d
Mogens Heller Grabe <mookid8000@gmail.com>
2012-05-12 23:47:21 +0200

    removed out-commented code

--- src/Rebus.MongoDb/MongoDbTimeoutStorage.cs
+++ src/Rebus.MongoDb/MongoDbTimeoutStorage.cs
@@ -58,27 +58,6 @@ namespace Rebus.MongoDb
                     gotResult = false;
                 }
             } while (gotResult);
-
-            //var timeout = collection.FindAndRemove(Query.LTE("time", Time.Now()), SortBy.Ascending("time"));
-
-            //var document = timeout.ModifiedDocument;
-
-            //if (document == null)
-            //{
-            //    return new Timeout.Timeout[0];
-            //}
-
-            //return new[]
-            //           {
-            //               new Timeout.Timeout
-            //                   {
-            //                       CorrelationId = document["corr_id"].AsString,
-            //                       SagaId = document["saga_id"].AsGuid,
-            //                       CustomData = document["data"] != BsonNull.Value ? document["data"].AsString : "",
-            //                       ReplyTo = document["reply_to"].AsString,
-            //                       TimeToReturn = document["time"].AsDateTime,
-            //                   }
-            //           };
         }
     }
 }
