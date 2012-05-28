20c8e5ac64912e0fc33ffe549dcaede14bc55ba9
clausndk <cn@clausn.dk>
2012-05-29 00:05:04 +0200

    Added XML subscription storage

---
+++ src/Rebus.Xml/XmlTimeoutStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rebus.Timeout;
+
+namespace Rebus.Xml
+{
+    public class XmlTimeoutStorage : IStoreTimeouts
+    {
+        public void Add(Timeout.Timeout newTimeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Timeout.Timeout> RemoveDueTimeouts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
