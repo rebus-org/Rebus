1ee5dc1b8ea19538d8e6f498ef249b035713e470
clausndk <cn@clausn.dk>
2012-06-03 11:30:11 +0200

    Added XML Subscription

---
+++ src/Rebus.Tests/Persistence/Subscriptions/Factories/XmlSubscriptionStoreFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Rebus.Tests.Persistence.Subscriptions.Factories
+{
+    public class XmlSubscriptionStoreFactory : ISubscriptionStoreFactory
+    {
+        public IStoreSubscriptions CreateStore()
+        {
+            var path = Environment.CurrentDirectory;
+            var filename = "subscriptions.xml";
+            var filePath = Path.Combine(path, filename);
+
+            var store = new Rebus.Xml.XmlSubscriptionStorage(filePath);
+            store.ClearAllSubscriptions();
+            return store;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
