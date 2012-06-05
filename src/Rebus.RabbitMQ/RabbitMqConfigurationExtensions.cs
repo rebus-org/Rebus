2c3b5b25edc484ffcaef5e2835c8f30c8946c01c
Mogens Heller Grabe <mookid8000@gmail.com>
2012-06-05 23:10:15 +0200

    added XML configuration support for Rabbit transport

--- src/Rebus.RabbitMQ/RabbitMqConfigurationExtensions.cs
+++ src/Rebus.RabbitMQ/RabbitMqConfigurationExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Rebus.Configuration.Configurers;
+﻿using System.Configuration;
+using Rebus.Configuration;
+using Rebus.Configuration.Configurers;
+using ConfigurationException = Rebus.Configuration.ConfigurationException;
 
 namespace Rebus.RabbitMQ
 {
@@ -6,9 +9,69 @@ namespace Rebus.RabbitMQ
     {
          public static void UseRabbitMq(this TransportConfigurer configurer, string connectionString, string inputQueueName, string errorQueue)
          {
-             var queue = new RabbitMqMessageQueue(connectionString, inputQueueName, errorQueue);
-             configurer.UseSender(queue);
-             configurer.UseReceiver(queue);
+             DoIt(configurer, connectionString, inputQueueName, errorQueue);
          }
+
+        public static void UseRabbitMqAndGetInputQueueNameFromAppConfig(this TransportConfigurer configurer, string connectionString)
+         {
+             try
+             {
+                 var section = RebusConfigurationSection.LookItUp();
+
+                 var inputQueueName = section.InputQueue;
+
+                 if (string.IsNullOrEmpty(inputQueueName))
+                 {
+                     throw new ConfigurationErrorsException("Could not get input queue name from Rebus configuration section. Did you forget the 'inputQueue' attribute?");
+                 }
+
+                 var errorQueueName = section.ErrorQueue;
+
+                 if (string.IsNullOrEmpty(errorQueueName))
+                 {
+                     throw new ConfigurationErrorsException("Could not get input queue name from Rebus configuration section. Did you forget the 'errorQueue' attribute?");
+                 }
+
+                 DoIt(configurer, connectionString, inputQueueName, errorQueueName);
+             }
+             catch (ConfigurationErrorsException e)
+             {
+                 throw new ConfigurationException(
+                     @"
+An error occurred when trying to parse out the configuration of the RebusConfigurationSection:
+
+{0}
+
+-
+
+For this way of configuring input queue to work, you need to supply a correct configuration
+section declaration in the <configSections> element of your app.config/web.config - like so:
+
+    <configSections>
+        <section name=""rebus"" type=""Rebus.Configuration.RebusConfigurationSection, Rebus"" />
+        <!-- other stuff in here as well -->
+    </configSections>
+
+-and then you need a <rebus> element some place further down the app.config/web.config,
+like so:
+
+    <rebus inputQueue=""my.service.input.queue"" errorQueue=""my.service.error.queue"" />
+
+Note also, that specifying the input queue name with the 'inputQueue' attribute is optional.
+
+A more full example configuration snippet can be seen here:
+
+{1}
+",
+                     e, RebusConfigurationSection.ExampleSnippetForErrorMessages);
+             }
+         }
+
+        static void DoIt(TransportConfigurer configurer, string connectionString, string inputQueueName, string errorQueueName)
+        {
+            var queue = new RabbitMqMessageQueue(connectionString, inputQueueName, errorQueueName);
+            configurer.UseSender(queue);
+            configurer.UseReceiver(queue);
+        }
     }
 }
