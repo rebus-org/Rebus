0ed67f3e509e49e5c758413a2bc1ce876ef0352c
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-08-08 23:03:57 +0200

    Basic NLog Logger

---
+++ src/Rebus.NLog/NLogLoggingExtension.cs
@@ -0,0 +1,13 @@
+﻿using Rebus.Configuration.Configurers;
+using Rebus.Logging;
+
+namespace Rebus.NLog
+{
+    public static class NLogLoggingExtension
+    {
+        public static void Log4Net(this LoggingConfigurer configurer)
+        {
+            RebusLoggerFactory.Current = new NLogLoggerFactory();
+        }
+    }
+}
