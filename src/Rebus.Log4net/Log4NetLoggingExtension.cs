5f360d599715cdd09133ec28d6907d737a1c6b01
Mogens Heller Grabe <mookid8000@gmail.com>
2012-12-04 14:02:28 +0100

    added XML

--- src/Rebus.Log4net/Log4NetLoggingExtension.cs
+++ src/Rebus.Log4net/Log4NetLoggingExtension.cs
@@ -2,8 +2,14 @@
 
 namespace Rebus.Log4Net
 {
+    /// <summary>
+    /// Extensions to <see cref="LoggingConfigurer"/> that allows for choosing Log4net for all of Rebus' internal logging needs
+    /// </summary>
     public static class Log4NetLoggingExtension
     {
+        /// <summary>
+        /// Configures Rebus to use Log4net for all of its internal logging
+        /// </summary>
         public static void Log4Net(this LoggingConfigurer configurer)
         {
             configurer.Use(new Log4NetLoggerFactory());
