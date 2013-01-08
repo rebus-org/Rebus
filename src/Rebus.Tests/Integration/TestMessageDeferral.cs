16e8855d89f6357fe11a3b2660948fe699925b4c
Mogens Heller Grabe <mookid8000@gmail.com>
2013-01-08 08:31:41 +0100

    doubled the accepted tolerance in message deferral test to account for sluggish build server

--- src/Rebus.Tests/Integration/TestMessageDeferral.cs
+++ src/Rebus.Tests/Integration/TestMessageDeferral.cs
@@ -71,7 +71,7 @@ namespace Rebus.Tests.Integration
             var messages = new List<Tuple<DateTime, DateTime>>();
             handlerActivator.Handle<MessageWithExpectedReturnTime>(m => messages.Add(new Tuple<DateTime, DateTime>(m.ExpectedReturnTime, DateTime.UtcNow)));
 
-            var acceptedTolerance = 2.Seconds();
+            var acceptedTolerance = 4.Seconds();
             var random = new Random();
 
             // act
