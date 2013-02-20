84867587d16277b2934d8c3dfaf4bd7b81bddcca
schourode <jsr@malamute.dk>
2013-02-20 11:05:55 +0100

    AutofacContainerAdapter uses a separate ILifetimeScope in each IUnitOfWork, ensuring that components registered in the container as InstancePerLifetimeScope() will not outlive the current unit of work.

--- src/Rebus.Autofac/AutofacContainerAdapter.cs
+++ src/Rebus.Autofac/AutofacContainerAdapter.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Autofac;
+using Rebus.Bus;
 using Rebus.Configuration;
 
 namespace Rebus.Autofac
 {
     public class AutofacContainerAdapter : IContainerAdapter
     {
+        const string ContextKey = "AutofacLifetimeScope";
+
         readonly IContainer container;
 
         public AutofacContainerAdapter(IContainer container)
@@ -23,15 +26,14 @@ namespace Rebus.Autofac
 
         public IEnumerable<IHandleMessages<T>> GetHandlerInstancesFor<T>()
         {
-            return container.Resolve<IEnumerable<IHandleMessages<T>>>().ToArray();
+            var context = MessageContext.GetCurrent();
+            var lifetimeScope = (ILifetimeScope)context.Items[ContextKey];
+
+            return lifetimeScope.Resolve<IEnumerable<IHandleMessages<T>>>().ToArray();
         }
 
         public void Release(IEnumerable handlerInstances)
         {
-            foreach (var disposable in handlerInstances.OfType<IDisposable>())
-            {
-                disposable.Dispose();
-            }
         }
 
         public void SaveBusInstances(IBus bus)
@@ -40,6 +42,50 @@ namespace Rebus.Autofac
             builder.RegisterInstance(bus).As<IBus>();
             builder.Register(a => MessageContext.GetCurrent()).InstancePerDependency();
             builder.Update(container);
+
+            bus.Advanced.Events.AddUnitOfWorkManager(new AutofacUnitOfWorkManager(container));
+        }
+
+        class AutofacUnitOfWorkManager : IUnitOfWorkManager
+        {
+            readonly IContainer container;
+
+            public AutofacUnitOfWorkManager(IContainer container)
+            {
+                this.container = container;
+            }
+
+            public IUnitOfWork Create()
+            {
+                var context = MessageContext.GetCurrent();
+                var lifetimeScope = container.BeginLifetimeScope();
+                context.Items.Add(ContextKey, lifetimeScope);
+
+                return new AutofacUnitOfWork(lifetimeScope);
+            }
+        }
+
+        class AutofacUnitOfWork : IUnitOfWork
+        {
+            readonly ILifetimeScope lifetimeScope;
+
+            public AutofacUnitOfWork(ILifetimeScope lifetimeScope)
+            {
+                this.lifetimeScope = lifetimeScope;
+            }
+
+            public void Commit()
+            {
+            }
+
+            public void Abort()
+            {
+            }
+
+            public void Dispose()
+            {
+                lifetimeScope.Dispose();
+            }
         }
     }
 }
