b8b63df2621738da73b6eb3732d9081f36b149ab
Mogens Heller Grabe <mookid8000@gmail.com>
2013-06-20 07:43:12 +0200

    added logging

--- src/Rebus.AzureServiceBus/AzureServiceBusMessageQueue.cs
+++ src/Rebus.AzureServiceBus/AzureServiceBusMessageQueue.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Threading;
 using System.Transactions;
 using Microsoft.ServiceBus;
 using Microsoft.ServiceBus.Messaging;
 using System.Linq;
+using Rebus.Logging;
 
 namespace Rebus.AzureServiceBus
 {
     public class AzureServiceBusMessageQueue : IDuplexTransport
     {
+        static ILog log;
+
+        static AzureServiceBusMessageQueue()
+        {
+            RebusLoggerFactory.Changed += f => log = f.GetCurrentClassLogger();
+        }
+
         const string TopicName = "Rebus";
         const string LogicalQueuePropertyKey = "LogicalDestinationQueue";
-        
+
         readonly NamespaceManager namespaceManager;
         readonly TopicDescription topicDescription;
         readonly TopicClient topicClient;
@@ -20,19 +29,26 @@ namespace Rebus.AzureServiceBus
 
         public AzureServiceBusMessageQueue(string connectionString, string inputQueue)
         {
+            log.Info("Initializing Azure Service Bus transport with logical input queue '{0}'", inputQueue);
+
             namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
-            
+
             InputQueue = inputQueue;
 
             try
             {
+                log.Info("Ensuring that topic '{0}' exists", TopicName);
                 namespaceManager.CreateTopic(TopicName);
             }
-            catch{}
+            catch { }
 
             topicDescription = namespaceManager.GetTopic(TopicName);
-            GetOrCreateSubscription(topicDescription, InputQueue);
+            GetOrCreateSubscription(InputQueue);
+
+            log.Info("Creating topic client");
             topicClient = TopicClient.CreateFromConnectionString(connectionString, topicDescription.Path);
+
+            log.Info("Creating subscription client");
             subscriptionClient = SubscriptionClient.CreateFromConnectionString(connectionString, TopicName, InputQueue);
         }
 
@@ -57,7 +73,71 @@ namespace Rebus.AzureServiceBus
 
             var brokeredMessage = new BrokeredMessage(envelope);
             brokeredMessage.Properties[LogicalQueuePropertyKey] = destinationQueueName;
-            topicClient.Send(brokeredMessage);
+
+            // if we're transactional, let the transaction happen and do its thing
+            if (context.IsTransactional)
+            {
+                topicClient.Send(brokeredMessage);
+                return;
+            }
+
+            new Retrier(TimeSpan.FromSeconds(.1),
+                        TimeSpan.FromSeconds(.2),
+                        TimeSpan.FromSeconds(.5),
+                        TimeSpan.FromSeconds(1),
+                        TimeSpan.FromSeconds(2))
+                .RetryOn<ServerBusyException>()
+                .RetryOn<MessagingCommunicationException>()
+                .RetryOn<TimeoutException>()
+                .Do(() => topicClient.Send(brokeredMessage));
+        }
+
+        class Retrier
+        {
+            readonly TimeSpan[] backoffs;
+            readonly List<Type> toleratedExceptionTypes = new List<Type>();
+
+            public Retrier(params TimeSpan[] backoffs)
+            {
+                this.backoffs = backoffs;
+            }
+
+            public Retrier RetryOn<TException>() where TException : Exception
+            {
+                toleratedExceptionTypes.Add(typeof(TException));
+                return this;
+            }
+
+            public void Do(Action action)
+            {
+                var backoffIndex = 0;
+                var complete = false;
+                
+                while (!complete)
+                {
+                    try
+                    {
+                        action();
+                        complete = true;
+                    }
+                    catch (Exception e)
+                    {
+                        if (backoffIndex >= backoffs.Length)
+                        {
+                            throw;
+                        }
+
+                        if (toleratedExceptionTypes.Contains(e.GetType()))
+                        {
+                            Thread.Sleep(backoffs[backoffIndex++]);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
         }
 
         public ReceivedTransportMessage ReceiveMessage(ITransactionContext context)
@@ -88,14 +168,14 @@ namespace Rebus.AzureServiceBus
                                              ? new Dictionary<string, object>()
                                              : envelope
                                                    .Headers
-                                                   .ToDictionary(e => e.Key, e => (object) e.Value),
+                                                   .ToDictionary(e => e.Key, e => (object)e.Value),
                                Body = envelope.Body,
                                Label = envelope.Label
                            };
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                log.Warn("Caught exception while receiving message: {0}", e);
                 return null;
             }
         }
@@ -106,8 +186,11 @@ namespace Rebus.AzureServiceBus
 
         public AzureServiceBusMessageQueue Purge()
         {
+            log.Warn("Purging logical queue {0}", InputQueue);
+
             namespaceManager.DeleteSubscription(topicDescription.Path, InputQueue);
-            GetOrCreateSubscription(topicDescription, InputQueue);
+            GetOrCreateSubscription(InputQueue);
+            
             return this;
         }
 
@@ -115,7 +198,7 @@ namespace Rebus.AzureServiceBus
         class Envelope
         {
             [DataMember]
-            public Dictionary<string,string> Headers { get; set; }
+            public Dictionary<string, string> Headers { get; set; }
 
             [DataMember]
             public byte[] Body { get; set; }
@@ -124,14 +207,15 @@ namespace Rebus.AzureServiceBus
             public string Label { get; set; }
         }
 
-        void GetOrCreateSubscription(TopicDescription topicDescription, string name)
+        void GetOrCreateSubscription(string logicalQueueName)
         {
-            if (namespaceManager.SubscriptionExists(topicDescription.Path, name)) return;
-            
+            if (namespaceManager.SubscriptionExists(topicDescription.Path, logicalQueueName)) return;
+
             try
             {
-                var filter = new SqlFilter(string.Format("LogicalDestinationQueue = '{0}'", name));
-                namespaceManager.CreateSubscription(topicDescription.Path, name, filter);
+                log.Info("Establishing subscription '{0}'", logicalQueueName);
+                var filter = new SqlFilter(string.Format("LogicalDestinationQueue = '{0}'", logicalQueueName));
+                namespaceManager.CreateSubscription(topicDescription.Path, logicalQueueName, filter);
             }
             catch (MessagingEntityAlreadyExistsException)
             {
---
+++ src/Rebus.Tests/Performance/TestRebusBusWithAzureServiceBusMessageQueue.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Rebus.Configuration;
+using Rebus.AzureServiceBus;
+using Rebus.Tests.Configuration;
+using Rebus.Tests.Contracts.Transports.Factories;
+using Rebus.Tests.Integration;
+using Shouldly;
+using Rebus.Logging;
+
+namespace Rebus.Tests.Performance
+{
+    [TestFixture, Category(TestCategories.Azure)]
+    public class TestRebusBusWithAzureServiceBusMessageQueue : FixtureBase
+    {
+        const string QueueName1 = "perftest.bus1";
+        const string QueueName2 = "perftest.bus2";
+        
+        BuiltinContainerAdapter adapter1;
+        BuiltinContainerAdapter adapter2;
+
+        protected override void DoSetUp()
+        {
+            PurgeQueue(QueueName1);
+            PurgeQueue(QueueName2);
+
+            adapter1 = new BuiltinContainerAdapter();
+
+            Configure.With(adapter1)
+                     .Logging(l => l.Console(minLevel: LogLevel.Warn))
+                     .Transport(t => t.UseAzureServiceBus(AzureServiceBusMessageQueueFactory.ConnectionString, QueueName1, "error"))
+                     .CreateBus()
+                     .Start();
+
+            adapter2 = new BuiltinContainerAdapter();
+
+            Configure.With(adapter2)
+                     .Logging(l => l.Console(minLevel: LogLevel.Warn))
+                     .Transport(t => t.UseAzureServiceBus(AzureServiceBusMessageQueueFactory.ConnectionString, QueueName2, "error"))
+                     .CreateBus()
+                     .Start();
+        }
+
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        public void CanSendAndReceiveManyMessagesReliably(int numberOfMessages)
+        {
+            var messages = Enumerable.Range(1, numberOfMessages)
+                                     .Select(n => new SomeNumberedMessage
+                                                      {
+                                                          Id = Guid.NewGuid(),
+                                                          Text = string.Format("Message # {0}", n)
+                                                      })
+                                     .ToList();
+
+            var receivedMessages = new ConcurrentList<SomeNumberedMessage>();
+            var resetEvent = new ManualResetEvent(false);
+            var sentMessageIds = new ConcurrentList<Guid>();
+
+            adapter2.Handle<SomeNumberedMessage>(msg =>
+                {
+                    receivedMessages.Add(msg);
+
+                    if (receivedMessages.Count%1000 == 0)
+                    {
+                        Console.WriteLine("Received {0} messages", receivedMessages.Count);
+                    }
+
+                    if (receivedMessages.Count >= numberOfMessages)
+                    {
+                        resetEvent.Set();
+                    }
+                });
+
+            var rebusRouting = adapter1.Bus.Advanced.Routing;
+            var stopwatch = Stopwatch.StartNew();
+
+            foreach (var msg in messages)
+            {
+                rebusRouting.Send(QueueName2, msg);
+
+                sentMessageIds.Add(msg.Id);
+
+                if (sentMessageIds.Count%1000 == 0)
+                {
+                    Console.WriteLine("Sent {0} messages", sentMessageIds.Count);
+                }
+            }
+
+            var totalSeconds = stopwatch.Elapsed.TotalSeconds;
+            Console.WriteLine("Sending {0} messages took {1:0.0} s - that's {2:0} msg/s",
+                              numberOfMessages, totalSeconds, numberOfMessages/totalSeconds);
+
+            var timeout = TimeSpan.FromSeconds(15 + numberOfMessages/40.0);
+            Console.WriteLine("Waiting with {0} timeout", timeout);
+            if (!resetEvent.WaitOne(timeout))
+            {
+                Assert.Fail(@"Did not receive all {0} messages within {1} timeout
+
+{2}",
+                            numberOfMessages, timeout, FormatReport(sentMessageIds, receivedMessages));
+            }
+
+            // wait and see if we have duplicates
+            Thread.Sleep(1000);
+
+            receivedMessages.Count.ShouldBe(numberOfMessages);
+
+            foreach (var id in sentMessageIds)
+            {
+                var messagesWithThatId = receivedMessages.Count(m => m.Id == id);
+
+                Assert.That(messagesWithThatId, Is.EqualTo(1),
+                            @"Expected exactly 1 message with ID {0}, but there was {1}!!
+
+{2}",
+                            id, messagesWithThatId, FormatReport(sentMessageIds, receivedMessages));
+            }
+        }
+
+        string FormatReport(ConcurrentList<Guid> sentMessageIds, ConcurrentList<SomeNumberedMessage> receivedMessages)
+        {
+            var sentButNotReceivedIds = sentMessageIds.Where(i => !receivedMessages.Any(m => m.Id == i)).ToList();
+            var duplicatedMessages = receivedMessages.GroupBy(m => m.Id)
+                                                     .Where(g => g.Count() > 1)
+                                                     .ToList();
+
+            return string.Format(@"The following messages were sent but not received: {0}
+(that was {1})
+
+The following received messages were duplicated: {2}
+(that was {3})",
+                                 string.Join(", ", sentButNotReceivedIds),
+                                 sentButNotReceivedIds.Count,
+                                 string.Join(", ", duplicatedMessages.Select(m => m.Key)),
+                                 duplicatedMessages.Count);
+        }
+
+        class SomeNumberedMessage
+        {
+            public string Text { get; set; }
+            public Guid Id { get; set; }
+        }
+
+        protected override void DoTearDown()
+        {
+            adapter1.Dispose();
+            adapter2.Dispose();
+        }
+
+        void PurgeQueue(string queueName)
+        {
+            new AzureServiceBusMessageQueue(AzureServiceBusMessageQueueFactory.ConnectionString, queueName).Purge();
+        }
+    }
+}
