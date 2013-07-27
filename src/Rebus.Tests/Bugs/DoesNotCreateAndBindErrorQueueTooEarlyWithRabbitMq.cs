8319014a606eca959e7b6cafa4bec8d261cc35b4
Mogens Heller Grabe <mookid8000@gmail.com>
2013-07-27 18:14:05 +0200

    fixed #183 where exchange and error queue would be prematurely declared, thus making it impossible to use additionally customized RabbitMQ options

---
+++ src/Rebus.Tests/Bugs/DoesNotCreateAndBindErrorQueueTooEarlyWithRabbitMq.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using Rebus.Bus;
+using Rebus.Configuration;
+using Rebus.Serialization.Json;
+using Rebus.Tests.Transports.Rabbit;
+using Rebus.RabbitMQ;
+using Shouldly;
+
+namespace Rebus.Tests.Bugs
+{
+    [TestFixture, Category(TestCategories.Rabbit)]
+    public class DoesNotCreateAndBindErrorQueueTooEarlyWithRabbitMq : RabbitMqFixtureBase
+    {
+        [Test]
+        public void ItHasBeenFixed()
+        {
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                adapter.Handle<string>(s =>
+                    {
+                        throw new ApplicationException("Will trigger that the message gets moved to the error queue");
+                    });
+
+                Configure.With(adapter)
+                         .Transport(t => t.UseRabbitMq(ConnectionString, "test.input", "test.error")
+                                          .UseExchange("AlternativeExchange"))
+                         .CreateBus()
+                         .Start();
+
+                adapter.Bus.SendLocal("hello there!!!");
+
+                // wait until we're sure
+                Thread.Sleep(2.Seconds());
+
+                using (var errorQueue = new RabbitMqMessageQueue(ConnectionString, "test.error"))
+                {
+                    var serializer = new JsonMessageSerializer();
+                    var receivedTransportMessage = errorQueue.ReceiveMessage(new NoTransaction());
+                    receivedTransportMessage.ShouldNotBe(null);
+
+                    var errorMessage = serializer.Deserialize(receivedTransportMessage);
+                    errorMessage.Messages.Length.ShouldBe(1);
+                    errorMessage.Messages[0].ShouldBeTypeOf<string>();
+                    ((string)errorMessage.Messages[0]).ShouldBe("hello there!!!");
+                }
+            }
+        }
+    }
+}
