e6becdb320a81b9e582cdea5137ee06f986e4027
Mogens Heller Grabe <mookid8000@gmail.com>
2013-07-30 08:17:04 +0200

    in-mem callback prototype

---
+++ src/Rebus.Tests/Integration/TestWebCallbacks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Messaging;
+using System.Threading;
+using NUnit.Framework;
+using Rebus.Configuration;
+using Rebus.Shared;
+using Rebus.Transports.Msmq;
+using Rebus.WebAsync;
+
+namespace Rebus.Tests.Integration
+{
+    [TestFixture]
+    public class TestWebCallbacks : FixtureBase, IDetermineMessageOwnership
+    {
+        const string QueueName = "test.webcallbacks.input1";
+
+        [Test]
+        public void ItWorks()
+        {
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                Configure.With(adapter)
+                         .Transport(t => t.UseMsmq(QueueName, "error"))
+                         .MessageOwnership(o => o.Use(this))
+                         .AllowWebCallbacks()
+                         .CreateBus()
+                         .Start();
+
+                var bus = adapter.Bus;
+                adapter.Handle<string>(s => bus.Reply("Thank you sir!"));
+
+                var resetEvent = new ManualResetEvent(false);
+
+                adapter.Bus.Send("hello there!", (string reply) =>
+                    {
+                        Console.WriteLine("Got reply in callback: {0} - YAY!", reply);
+                        resetEvent.Set();
+                    });
+
+                if (!resetEvent.WaitOne(2.Seconds()))
+                {
+                    Assert.Fail("Did not receive reply withing 2 seconds of waiting!");
+                }
+            }
+        }
+
+        [Test]
+        public void HandlesTypeMismatchGracefully()
+        {
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                Configure.With(adapter)
+                         .Transport(t => t.UseMsmq(QueueName, "error"))
+                         .MessageOwnership(o => o.Use(this))
+                         .AllowWebCallbacks()
+                         .CreateBus()
+                         .Start();
+
+                var bus = adapter.Bus;
+                adapter.Handle<string>(s => bus.Reply("Thank you sir!"));
+
+                var resetEvent = new ManualResetEvent(false);
+
+                adapter.Bus.Send("hello there!", (DateTime reply) =>
+                    {
+                        resetEvent.Set();
+                    });
+
+                if (!resetEvent.WaitOne(2.Seconds()))
+                {
+                    Assert.Fail("Did not receive reply withing 2 seconds of waiting!");
+                }
+            }
+        }
+
+        protected override void DoTearDown()
+        {
+            MessageQueue.Delete(MsmqUtil.GetPath(QueueName));
+        }
+
+        public string GetEndpointFor(Type messageType)
+        {
+            if (messageType == typeof (string))
+                return QueueName;
+
+            throw new ArgumentException(string.Format("Don't know where to send {0}", messageType));
+        }
+    }
+}
---
+++ src/Rebus.WebAsync/ConfigurationEx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Rebus.Configuration;
+using Rebus.Shared;
+using System.Linq;
+
+namespace Rebus.WebAsync
+{
+    public static class ConfigurationEx
+    {
+        static internal ConcurrentDictionary<string, Delegate> registeredReplyHandlers = new ConcurrentDictionary<string, Delegate>();
+
+        public static RebusConfigurer AllowWebCallbacks(this RebusConfigurer configurer)
+        {
+            configurer.AddDecoration(b =>
+                {
+                    b.ActivateHandlers = new CorrelationHandlerInjector(b.ActivateHandlers);
+                });
+            return configurer;
+        }
+
+        class CorrelationHandlerInjector : IActivateHandlers, IDisposable
+        {
+            readonly IActivateHandlers innerActivator;
+
+            public CorrelationHandlerInjector(IActivateHandlers innerActivator)
+            {
+                this.innerActivator = innerActivator;
+            }
+
+            public IEnumerable<IHandleMessages<T>> GetHandlerInstancesFor<T>()
+            {
+                return innerActivator.GetHandlerInstancesFor<T>()
+                                     .Concat(new[] {new ReplyMessageHandler<T>(registeredReplyHandlers)});
+            }
+
+            public void Release(IEnumerable handlerInstances)
+            {
+                innerActivator.Release(handlerInstances);
+            }
+
+            public void Dispose()
+            {
+                var activator = innerActivator as IDisposable;
+                if (activator == null) return;
+                
+                activator.Dispose();
+            }
+        }
+
+        public static void Send<TReply>(this IBus bus, object message, Action<TReply> replyHandler)
+        {
+            var correlationId = Guid.NewGuid().ToString();
+            bus.AttachHeader(message, Headers.CorrelationId, correlationId);
+            registeredReplyHandlers.TryAdd(correlationId, replyHandler);
+            bus.Send(message);
+        }
+    }
+
+    public class ReplyMessageHandler<T> : IHandleMessages<T>
+    {
+        readonly ConcurrentDictionary<string, Delegate> registeredReplyHandlers;
+
+        public ReplyMessageHandler(ConcurrentDictionary<string, Delegate> registeredReplyHandlers)
+        {
+            this.registeredReplyHandlers = registeredReplyHandlers;
+        }
+
+        public void Handle(T message)
+        {
+            var currentHeaders = MessageContext.GetCurrent().Headers;
+            if (!currentHeaders.ContainsKey(Headers.CorrelationId)) return;
+
+            var correlationId = currentHeaders[Headers.CorrelationId].ToString();
+            
+            Delegate callback;
+            if (!registeredReplyHandlers.TryRemove(correlationId, out callback))
+            {
+                return;
+            }
+
+            try
+            {
+                callback.DynamicInvoke(message);
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationException(string.Format("An error occurred while attempting to dispatch received reply {0} with correlation ID {1} to callback {2}",
+                    message, correlationId, callback), e);
+            }
+        }
+    }
+}
