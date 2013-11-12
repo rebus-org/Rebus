e8ff35eb6ce02e6f91ef1aa979210fb161b7aee9
Mogens Heller Grabe <mookid8000@gmail.com>
2013-11-12 09:47:43 +0100

    added a bunch of comments to Rabbit transport

--- src/Rebus.RabbitMQ/RabbitMqMessageQueue.cs
+++ src/Rebus.RabbitMQ/RabbitMqMessageQueue.cs
@@ -18,8 +18,14 @@ namespace Rebus.RabbitMQ
     /// </summary>
     public class RabbitMqMessageQueue : IMulticastTransport, IDisposable, INeedInitializationBeforeStart
     {
+        /// <summary>
+        /// Headers that affect how Rabbit actually transports the messages
+        /// </summary>
         public static class InternalHeaders
         {
+            /// <summary>
+            /// Affects whether Rabbit messages are sent with the "persistent" setting on or off
+            /// </summary>
             public const string MessageDurability = "rmq-msg-durability";
         }
 
@@ -56,11 +62,20 @@ namespace Rebus.RabbitMQ
         [ThreadStatic]
         static Subscription threadBoundSubscription;
 
+        /// <summary>
+        /// Constructs the transport with the given connection string in "send-only" mode. This means that
+        /// the transport can only send messages (note: and publish if Rabbit manages the subscriptions)
+        /// </summary>
         public static RabbitMqMessageQueue Sender(string connectionString)
         {
             return new RabbitMqMessageQueue(connectionString, null);
         }
 
+        /// <summary>
+        /// Constructs the transport with the given connection string and input queue name. Depending on
+        /// further configuration, exchange, input queue, and bindings may/may not be initialized when the
+        /// transport is used.
+        /// </summary>
         public RabbitMqMessageQueue(string connectionString, string inputQueueName)
         {
             connectionManager = new ConnectionManager(connectionString, inputQueueName);
@@ -69,8 +84,27 @@ namespace Rebus.RabbitMQ
             this.inputQueueName = inputQueueName;
         }
 
+        /// <summary>
+        /// Initializes the transport by open a connection, setting up input queue etc
+        /// </summary>
+        public void Initialize()
+        {
+            if (SenderOnly) return;
+
+            using (var model = GetConnection().CreateModel())
+            {
+                InitializeLogicalQueue(inputQueueName, model, autoDeleteInputQueue);
+            }
+        }
+
         bool SenderOnly { get { return string.IsNullOrEmpty(inputQueueName); } }
 
+        /// <summary>
+        /// Sends the specified message to the queue specified by <see cref="destinationQueueName"/>. Please
+        /// note that <see cref="destinationQueueName"/> is not actually a queue, it's a topic. That topic
+        /// MAY correspond to one single queue (in case it's the "default binding" for a queue), or it may
+        /// correspond to a "proper topic" (i.e. one that is meant to used for multicasting).
+        /// </summary>
         public void Send(string destinationQueueName, TransportMessageToSend message, ITransactionContext context)
         {
             try
@@ -101,6 +135,9 @@ namespace Rebus.RabbitMQ
             }
         }
 
+        /// <summary>
+        /// Attempts to receive a message, returning null if no message was available
+        /// </summary>
         public ReceivedTransportMessage ReceiveMessage(ITransactionContext context)
         {
             try
@@ -215,6 +252,9 @@ namespace Rebus.RabbitMQ
             get { return managesSubscriptions; }
         }
 
+        /// <summary>
+        /// Disposes the connection manager, thereby closing & disposing Rabbit connections
+        /// </summary>
         public void Dispose()
         {
             if (disposed) return;
@@ -264,6 +304,10 @@ namespace Rebus.RabbitMQ
             }
         }
 
+        /// <summary>
+        /// When managing subscriptions, the transport will be called when subscribing and ubsubscribing.
+        /// This will result in binding and unbinding, respectively, to the topic for the given <see cref="messageType"/>
+        /// </summary>
         public void Subscribe(Type eventType, string inputQueueAddress)
         {
             if (SenderOnly)
@@ -311,23 +355,10 @@ namespace Rebus.RabbitMQ
             }
         }
 
-        void EnsureInputQueueInitialized(string inputQueueNameToInitialize)
-        {
-            if (CanUseThreadBoundModel)
-            {
-                InitializeLogicalQueue(inputQueueNameToInitialize, threadBoundModel, autoDeleteInputQueue);
-            }
-            else
-            {
-                WithConnection(model => InitializeLogicalQueue(inputQueueNameToInitialize, model, autoDeleteInputQueue));
-            }
-        }
-
-        static bool CanUseThreadBoundModel
-        {
-            get { return threadBoundModel != null && threadBoundModel.IsOpen; }
-        }
-
+        /// <summary>
+        /// When managing subscriptions, the transport will be called when subscribing and ubsubscribing.
+        /// This will result in binding and unbinding, respectively, to the topic for the given <see cref="messageType"/>
+        /// </summary>
         public void Unsubscribe(Type messageType, string inputQueueAddress)
         {
             if (SenderOnly)
@@ -371,6 +402,10 @@ namespace Rebus.RabbitMQ
             }
         }
 
+        /// <summary>
+        /// Gets the event name for the given message type. This will be used as the topic when the given
+        /// message type is published
+        /// </summary>
         public string GetEventName(Type messageType)
         {
             foreach (var tryResolve in eventNameResolvers)
@@ -384,6 +419,10 @@ namespace Rebus.RabbitMQ
             return GetPrettyTypeName(messageType);
         }
 
+        /// <summary>
+        /// Instructs the transport that it should function as a subscription storage and delegate multicast
+        /// operations to the transport layer.
+        /// </summary>
         public RabbitMqMessageQueue ManageSubscriptions()
         {
             log.Info("RabbitMQ will manage subscriptions");
@@ -391,6 +430,11 @@ namespace Rebus.RabbitMQ
             return this;
         }
 
+        /// <summary>
+        /// Configures the transport to NOT create the default binding to the topic with the same name
+        /// as the input queue. Please note that the binding may already exist (possibly from a previous run)
+        /// which means that you have do recreate the queue or remove the binding manually.
+        /// </summary>
         public RabbitMqMessageQueue DoNotBindDefaultTopicToInputQueue()
         {
             log.Info("Will not bind default topic {0} to input queue {1}", inputQueueName, inputQueueName);
@@ -398,6 +442,10 @@ namespace Rebus.RabbitMQ
             return this;
         }
 
+        /// <summary>
+        /// Refrain from declaring the exchange when starting up - allows you to minimize how much the
+        /// Rabbit transport may interfere with an already-existing exchange
+        /// </summary>
         public RabbitMqMessageQueue DoNotDeclareExchange()
         {
             log.Info("Will not automatically (re)declare exchange");
@@ -405,6 +453,9 @@ namespace Rebus.RabbitMQ
             return this;
         }
 
+        /// <summary>
+        /// Instructs the Rabbit transport to publish messages on the given exchange
+        /// </summary>
         public RabbitMqMessageQueue UseExchange(string exchangeNameToUse)
         {
             log.Info("Will use exchanged named {0}", exchangeNameToUse);
@@ -412,6 +463,9 @@ namespace Rebus.RabbitMQ
             return this;
         }
 
+        /// <summary>
+        /// Configures Rabbit transport to prefetch the specified number of messages
+        /// </summary>
         public RabbitMqMessageQueue Prefetch(ushort prefetchCountToSet)
         {
             log.Info("Will set prefetch count to {0} on new connections", prefetchCountToSet);
@@ -419,11 +473,32 @@ namespace Rebus.RabbitMQ
             return this;
         }
 
+        /// <summary>
+        /// Configures Rabbit transport to create its input queue with the auto-delete
+        /// flag set
+        /// </summary>
         public RabbitMqMessageQueue AutoDeleteInputQueue()
         {
             log.Info("Will set the autodelete flag on input queue");
             autoDeleteInputQueue = true;
             return this;
+        }
+
+        void EnsureInputQueueInitialized(string inputQueueNameToInitialize)
+        {
+            if (CanUseThreadBoundModel)
+            {
+                InitializeLogicalQueue(inputQueueNameToInitialize, threadBoundModel, autoDeleteInputQueue);
+            }
+            else
+            {
+                WithConnection(model => InitializeLogicalQueue(inputQueueNameToInitialize, model, autoDeleteInputQueue));
+            }
+        }
+
+        static bool CanUseThreadBoundModel
+        {
+            get { return threadBoundModel != null && threadBoundModel.IsOpen; }
         }
 
         IModel GetSenderModel(ITransactionContext context)
@@ -461,30 +536,29 @@ namespace Rebus.RabbitMQ
             model.QueueBind(InputQueueAddress, ExchangeName, topic);
         }
 
-        static string GetPrettyTypeName(Type messageType)
+        string GetPrettyTypeName(Type messageType)
         {
-            if (messageType.IsGenericType)
+            if (!messageType.IsGenericType) 
+                return messageType.FullName;
+
+            var genericTypeDefinition = messageType.GetGenericTypeDefinition();
+            var genericArguments = messageType.GetGenericArguments();
+
+            var fullName = genericTypeDefinition.FullName;
+            var substring = fullName.Substring(0, fullName.IndexOf("`"));
+
+            var builder = new StringBuilder();
+            builder.Append(substring);
+            builder.Append("<");
+            var first = true;
+            foreach (var genericArgument in genericArguments)
             {
-                var genericTypeDefinition = messageType.GetGenericTypeDefinition();
-                var genericArguments = messageType.GetGenericArguments();
-
-                var fullName = genericTypeDefinition.FullName;
-                var substring = fullName.Substring(0, fullName.IndexOf("`"));
-
-                var builder = new StringBuilder();
-                builder.Append(substring);
-                builder.Append("<");
-                var first = true;
-                foreach (var genericArgument in genericArguments)
-                {
-                    if (!first) builder.Append(", ");
-                    builder.Append(GetPrettyTypeName(genericArgument));
-                    first = false;
-                }
-                builder.Append(">");
-                return builder.ToString();
+                if (!first) builder.Append(", ");
+                builder.Append(GetPrettyTypeName(genericArgument));
+                first = false;
             }
-            return messageType.FullName;
+            builder.Append(">");
+            return builder.ToString();
         }
 
         void InitializeLogicalQueue(string queueName, IModel model, bool autoDelete = false)
@@ -515,16 +589,6 @@ namespace Rebus.RabbitMQ
         internal void CreateQueue(string queueName)
         {
             WithConnection(model => InitializeLogicalQueue(queueName, model));
-        }
-
-        public void Initialize()
-        {
-            if (SenderOnly) return;
-
-            using (var model = GetConnection().CreateModel())
-            {
-                InitializeLogicalQueue(inputQueueName, model, autoDeleteInputQueue);
-            }
         }
 
         void EnsureThreadBoundModelIsInitialized(ITransactionContext context)
@@ -559,7 +623,7 @@ namespace Rebus.RabbitMQ
             EstablishSubscriptions(threadBoundModel);
         }
 
-        static ReceivedTransportMessage GetReceivedTransportMessage(IBasicProperties basicProperties, byte[] body)
+        ReceivedTransportMessage GetReceivedTransportMessage(IBasicProperties basicProperties, byte[] body)
         {
             var result = new ReceivedTransportMessage
                 {
@@ -590,7 +654,7 @@ namespace Rebus.RabbitMQ
             return result;
         }
 
-        static IDictionary<string, object> GetHeaders(IBasicProperties basicProperties)
+        IDictionary<string, object> GetHeaders(IBasicProperties basicProperties)
         {
             var headers = basicProperties.Headers;
 
@@ -599,7 +663,7 @@ namespace Rebus.RabbitMQ
             return headers.ToDictionary(de => (string)de.Key, de => PossiblyDecode(de.Value));
         }
 
-        static IBasicProperties GetHeaders(IModel modelToUse, TransportMessageToSend message)
+        IBasicProperties GetHeaders(IModel modelToUse, TransportMessageToSend message)
         {
             var props = modelToUse.CreateBasicProperties();
 
@@ -680,14 +744,14 @@ namespace Rebus.RabbitMQ
             return props;
         }
 
-        static object PossiblyEncode(object value)
+        object PossiblyEncode(object value)
         {
             if (!(value is string)) return value;
 
             return Encoding.GetBytes((string)value);
         }
 
-        static object PossiblyDecode(object value)
+        object PossiblyDecode(object value)
         {
             if (!(value is byte[])) return value;
 
