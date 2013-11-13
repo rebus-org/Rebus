0dec0d260c7455c649a2d9afc9503e27fc9dbd82
Mogens Heller Grabe <mookid8000@gmail.com>
2013-11-13 23:08:54 +0100

    since we're in a handler, and queue interaction is transactional, we should not handle exceptions

--- src/Rebus/Configuration/ConfigurationBackbone.cs
+++ src/Rebus/Configuration/ConfigurationBackbone.cs
@@ -216,15 +216,15 @@ namespace Rebus.Configuration
             forwardableMessage.Headers[Headers.AuditSourceQueue] = rebusBus.GetInputQueueAddress();
             forwardableMessage.Headers[Headers.AuditMessageProcessedTime] = RebusTimeMachine.Now().ToString("u");
 
-            try
-            {
+            //try
+            //{
                 rebusBus.InternalSend(auditQueueName, forwardableMessage, TransactionContext.Current);
-            }
-            catch (Exception exception)
-            {
-                log.Warn("An error occurred while attempting to copy message with ID {0} to {1}: {2}",
-                    message.Id, auditQueueName, exception);
-            }
+            //}
+            //catch (Exception exception)
+            //{
+            //    log.Warn("An error occurred while attempting to copy message with ID {0} to {1}: {2}",
+            //        message.Id, auditQueueName, exception);
+            //}
         }
     }
 }
