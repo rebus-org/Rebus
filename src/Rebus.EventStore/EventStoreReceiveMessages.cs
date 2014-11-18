b6bc26279e5dba419fc915819791bdeb300687e0
Fredrik Skeel Løkke <floekke@gmail.com>
2014-11-18 11:54:32 +0100

    updated newtonsoft json library, added Rebus.EventStore project and basic implementation

---
+++ src/Rebus.EventStore/EventStoreReceiveMessages.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Concurrent;
+using System.Text;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.SystemData;
+using Newtonsoft.Json;
+
+namespace Rebus.EventStore
+{
+    public class EventStoreReceiveMessages : IReceiveMessages
+    {
+        readonly IEventStoreConnection connection;
+        readonly ConcurrentQueue<MessageContainer> receivedMessages = new ConcurrentQueue<MessageContainer>();
+        EventStorePersistentSubscription subscription;
+        public string InputQueue { get; private set; }
+        public string InputQueueAddress { get; private set; }
+        string applicationId;
+
+        public EventStoreReceiveMessages(string applicationId, string inputQueue, IEventStoreConnection connection)
+        {
+            if (applicationId == null) throw new ArgumentNullException("applicationId");
+            if (inputQueue == null) throw new ArgumentNullException("inputQueue");
+            if (connection == null) throw new ArgumentNullException("connection");
+
+            this.applicationId = applicationId;
+            this.connection = connection;
+            InputQueue = inputQueue;
+            InputQueueAddress = inputQueue;
+
+            SubscribeToInputQueue();
+        }
+
+        private void SubscribeToInputQueue()
+        {
+            var settings = PersistentSubscriptionSettingsBuilder.Create()
+                .StartFromCurrent()
+                .PreferDispatchToSingle()
+                .WithMaxRetriesOf(5)
+                .Build();
+
+            try
+            {
+                var result = connection.CreatePersistentSubscriptionAsync(InputQueue, applicationId, settings,
+                    new UserCredentials("admin", "changeit")).Result;
+            }
+            catch (AggregateException)
+            {
+                // TODO: inspect and make sure its the right exception..
+            }
+           
+            subscription = connection.ConnectToPersistentSubscription(applicationId, InputQueue, EventAppeared, autoAck: false);
+        }
+
+        private void EventAppeared(EventStorePersistentSubscription subscription, ResolvedEvent evt)
+        {
+            var originalTransportMessage = JsonConvert.DeserializeObject<TransportMessageToSend>(Encoding.UTF8.GetString(evt.OriginalEvent.Data));
+            var message = new ReceivedTransportMessage {Id = evt.OriginalEvent.EventId.ToString("N"), Body = originalTransportMessage.Body, Headers = originalTransportMessage.Headers, Label = originalTransportMessage.Label };
+            receivedMessages.Enqueue(new MessageContainer { ReceivedTransportMessage = message, ResolvedEvent = evt });
+        }
+
+        public ReceivedTransportMessage ReceiveMessage(ITransactionContext context)
+        {
+            if (receivedMessages.IsEmpty)
+            {
+                return null;
+            }
+
+            MessageContainer message;
+            if (receivedMessages.TryDequeue(out message) == false)
+            {
+                return null;
+            }
+
+            // Leaking handlers?
+            if (context.IsTransactional)
+            {
+                context.DoCommit += () => subscription.Acknowledge(message.ResolvedEvent);
+
+                context.DoRollback +=
+                    () =>
+                    {
+                        //receivedMessages.Enqueue(message); 
+                        subscription.Fail(message.ResolvedEvent, PersistentSubscriptionNakEventAction.Retry, "Rebus transaction rolled back..");
+                    };
+            }
+
+            return message.ReceivedTransportMessage;
+        }
+
+        private class MessageContainer
+        {
+            public ReceivedTransportMessage ReceivedTransportMessage { get; set; }
+            public ResolvedEvent ResolvedEvent { get; set; }
+        }
+    }
+}
---
+++ src/Rebus.EventStore/EventStoreSendMessages.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Text;
+using EventStore.ClientAPI;
+using Newtonsoft.Json;
+
+namespace Rebus.EventStore
+{
+    public class EventStoreSendMessages : ISendMessages
+    {
+        readonly IEventStoreConnection connection;
+
+        public EventStoreSendMessages(IEventStoreConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Send(string destination, TransportMessageToSend message, ITransactionContext context)
+        {
+            if (destination == null) throw new ArgumentNullException("destination");
+            if(destination.Contains("-")) throw new ArgumentException("destination stream cannot contain -");
+
+            if (context.IsTransactional)
+            {
+                var transaction = connection.StartTransactionAsync(destination, ExpectedVersion.Any).Result;transaction.WriteAsync(CreateMessage(message)).Wait();
+
+                context.DoCommit += () => transaction.CommitAsync().Wait();
+                context.DoRollback += transaction.Rollback;
+            }
+            else
+            {
+                AppendToStream(destination, message);    
+            }
+        }
+
+        private void AppendToStream(string destination, TransportMessageToSend message)
+        {
+            var result = connection.AppendToStreamAsync(destination, ExpectedVersion.Any, CreateMessage(message)).Result;
+        }
+
+        private EventData CreateMessage(TransportMessageToSend message)
+        {
+            var messageAsJson = Encoding.UTF8.GetBytes((string) JsonConvert.SerializeObject(message));
+            return new EventData(Guid.NewGuid(), message.GetType().ToString(), true, messageAsJson, null);
+        }
+    }
+}
