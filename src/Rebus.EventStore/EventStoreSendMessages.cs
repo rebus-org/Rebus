e6cf38ad38ac42c2ddb313306338ba39f02eaec3
Fredrik Skeel Løkke <floekke@gmail.com>
2014-11-25 19:00:31 +0100

    implements the contract:
    if context has current transaction
        send using current transcation
    else
        start new transaction
        attach to commit/rollback/cleanup events
        send using the transaction
        stash the transaction in the context

--- src/Rebus.EventStore/EventStoreSendMessages.cs
+++ src/Rebus.EventStore/EventStoreSendMessages.cs
@@ -1,10 +1,12 @@
-using System;
-using System.Text;
 using EventStore.ClientAPI;
 using Newtonsoft.Json;
+using System;
+using System.Text;
 
 namespace Rebus.EventStore
 {
+    // TODO: rename *Wait* methods to *Synchrounous* or make async!
+
     public class EventStoreSendMessages : ISendMessages
     {
         readonly IEventStoreConnection connection;
@@ -16,32 +18,77 @@ namespace Rebus.EventStore
 
         public void Send(string destination, TransportMessageToSend message, ITransactionContext context)
         {
-            var eventStoreQueue = new EventStoreQueueIdentifier(destination);
+            destination = new EventStoreQueueIdentifier(destination).StreamId;
 
-            if (destination == null) throw new ArgumentNullException("destination");
-            if(destination.Contains("-")) throw new ArgumentException("destination stream cannot contain -");
-
-            if (context.IsTransactional)
+            if (context.IsTransactional && TransactionAlreadyStarted(context))
             {
-                var transaction = connection.StartTransactionAsync(destination, ExpectedVersion.Any).Result;transaction.WriteAsync(CreateMessage(message)).Wait();
+                WriteInTransactionAndWait(message, CurrentTransaction(context));
+            }
+            else if (context.IsTransactional && TransactionIsFresh(context))
+            {
+                var transaction = StartNewEventStoreTransaction(destination);
+                
+                SetCurrentTransaction(context, transaction);
+                
+                SetupTransactionEventHandlers(context, transaction);
 
-                context.DoCommit += () => transaction.CommitAsync().Wait();
-                context.DoRollback += transaction.Rollback;
+                WriteInTransactionAndWait(message, transaction);
             }
             else
             {
-                AppendToStream(destination, message);    
+                WriteAndWait(destination, message);
             }
         }
 
-        private void AppendToStream(string destination, TransportMessageToSend message)
+        private void SetupTransactionEventHandlers(ITransactionContext context, EventStoreTransaction transaction)
+        {
+            context.DoCommit += () => transaction.CommitAsync().Wait();
+            context.DoRollback += transaction.Rollback;
+            context.AfterRollback += () => SetCurrentTransaction(context, null);
+        }
+
+        private EventStoreTransaction StartNewEventStoreTransaction(string destination)
+        {
+            return connection.StartTransactionAsync(destination, ExpectedVersion.Any).Result;
+        }
+
+        private void WriteInTransactionAndWait(TransportMessageToSend message, EventStoreTransaction transaction)
+        {
+            transaction.WriteAsync(CreateMessage(message)).Wait();
+        }
+
+        void SetCurrentTransaction(ITransactionContext context, EventStoreTransaction transaction)
+        {
+            if (TransactionIsFresh(context) == false) throw new InvalidOperationException("Overriding existing transaction!");
+
+            context["singleTransaction"] = transaction;
+        }
+
+        bool TransactionIsFresh(ITransactionContext context)
+        {
+            return context["singleTransaction"] == null;
+        }
+
+        bool TransactionAlreadyStarted(ITransactionContext context)
+        {
+            return context["singleTransaction"] != null;
+        }
+
+        EventStoreTransaction CurrentTransaction(ITransactionContext context)
+        {
+            if(TransactionAlreadyStarted(context) == false) throw new InvalidOperationException("No existing transaction!");
+
+            return context["singleTransaction"] as EventStoreTransaction;
+        }
+
+        void WriteAndWait(string destination, TransportMessageToSend message)
         {
             var result = connection.AppendToStreamAsync(destination, ExpectedVersion.Any, CreateMessage(message)).Result;
         }
 
-        private EventData CreateMessage(TransportMessageToSend message)
+        EventData CreateMessage(TransportMessageToSend message)
         {
-            var messageAsJson = Encoding.UTF8.GetBytes((string) JsonConvert.SerializeObject(message));
+            var messageAsJson = Encoding.UTF8.GetBytes((string)JsonConvert.SerializeObject(message));
             return new EventData(Guid.NewGuid(), message.GetType().ToString(), true, messageAsJson, null);
         }
     }
