7d4fccc5e19d9deb6ec43ad168fdd99ca0af8d51
Mogens Heller Grabe <mookid8000@gmail.com>
2014-12-11 21:33:19 +0100

    removed silly exception rituals to achieve 100% portability

--- Playground/ResolutionException.cs
+++ Playground/ResolutionException.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Playground
 {
-    [Serializable]
     public class ResolutionException : Exception
     {
         public ResolutionException(string message, params object[] objs)
             : base(string.Format(message, objs))
-        {
-        }
-
-        public ResolutionException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
