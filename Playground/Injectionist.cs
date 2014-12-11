33f05751878cb69236132b7df12a7606cafed7bf
Mogens Heller Grabe <mookid8000@gmail.com>
2014-12-11 21:28:31 +0100

    simple decorators work now

--- Playground/Injectionist.cs
+++ Playground/Injectionist.cs
@@ -6,26 +6,63 @@ namespace Playground
 {
     public class Injectionist
     {
-        readonly Dictionary<Type, List<Delegate>> _resolvers = new Dictionary<Type, List<Delegate>>();
+        readonly Dictionary<Type, List<Resolver>> _resolvers = new Dictionary<Type, List<Resolver>>();
 
         public TService Get<TService>()
         {
             return new ResolutionContext(_resolvers).Get<TService>();
         }
 
-        public void Register<TService>(Func<IResolutionContext, TService> resolver)
+        public void Register<TService>(Func<IResolutionContext, TService> resolverMethod, bool isDecorator = false)
         {
-            if (!_resolvers.ContainsKey(typeof (TService)))
-                _resolvers.Add(typeof (TService), new List<Delegate>());
+            if (!_resolvers.ContainsKey(typeof(TService)))
+            {
+                _resolvers.Add(typeof(TService), new List<Resolver>());
+            }
 
-            _resolvers[typeof (TService)].Add(resolver);
+            var resolverList = _resolvers[typeof(TService)];
+            var resolver = new Resolver<TService>(resolverMethod, isDecorator: isDecorator);
+
+            if (!resolver.IsDecorator)
+            {
+                resolverList.Add(resolver);
+            }
+            else
+            {
+                resolverList.Insert(0, resolver);
+            }
+        }
+
+        abstract class Resolver { }
+
+        class Resolver<TService> : Resolver
+        {
+            readonly Func<IResolutionContext, TService> _resolver;
+            readonly bool _isDecorator;
+
+            public Resolver(Func<IResolutionContext, TService> resolver, bool isDecorator)
+            {
+                _resolver = resolver;
+                _isDecorator = isDecorator;
+            }
+
+            public bool IsDecorator
+            {
+                get { return _isDecorator; }
+            }
+
+            public TService InvokeResolver(IResolutionContext context)
+            {
+                return _resolver(context);
+            }
         }
 
         class ResolutionContext : IResolutionContext
         {
-            readonly Dictionary<Type, List<Delegate>> _resolvers;
+            readonly Dictionary<Type, int> _decoratorDepth = new Dictionary<Type, int>();
+            readonly Dictionary<Type, List<Resolver>> _resolvers;
 
-            public ResolutionContext(Dictionary<Type, List<Delegate>> resolvers)
+            public ResolutionContext(Dictionary<Type, List<Resolver>> resolvers)
             {
                 _resolvers = resolvers;
             }
@@ -37,11 +74,27 @@ namespace Playground
                     throw new ApplicationException(string.Format("Could not find resolver for {0}", typeof(TService)));
                 }
 
+                if (!_decoratorDepth.ContainsKey(typeof (TService)))
+                {
+                    _decoratorDepth[typeof (TService)] = 0;
+                }
+
                 var resolversForThisType = _resolvers[typeof(TService)];
 
-                return resolversForThisType
-                    .Cast<Func<IResolutionContext, TService>>()
-                    .First()(this);
+                try
+                {
+                    var depth = _decoratorDepth[typeof (TService)]++;
+
+                    return resolversForThisType
+                        .Cast<Resolver<TService>>()
+                        .Skip(depth)
+                        .First()
+                        .InvokeResolver(this);
+                }
+                finally
+                {
+                    _decoratorDepth[typeof (TService)]--;
+                }
             }
         }
     }
