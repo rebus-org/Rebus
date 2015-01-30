2d3ae85eca405f520e64e63f467b894dbf57524a
DixonD-git <dixond@acm.lviv.ua>
2015-01-30 22:25:46 +0200

    Added interfaces for async transport

---
+++ src/Rebus/IReceiveMessagesAsync.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace Rebus
+{
+    /// <summary>
+    /// Interface of something that is capable of receiving messages asynchronously. If no message is available,
+    /// null should be returned. If the bus is configured to run with multiple threads, this one
+    /// should be reentrant.
+    /// </summary>
+    public interface IReceiveMessagesAsync
+    {
+        /// <summary>
+        /// Attempt to receive the next available message asynchronously. Should return null if no message is available.
+        /// </summary>
+        Task<ReceivedTransportMessage> ReceiveMessage(ITransactionContext context);
+
+        /// <summary>
+        /// Gets the name of this receiver's input queue - i.e. this is the queue that this receiver
+        /// will pull messages from.
+        /// </summary>
+        string InputQueue { get; }
+
+        /// <summary>
+        /// Gets the globally accessible adddress of this receiver's input queue - i.e. this would probably
+        /// be the input queue in some form, possible qualified by machine name or something similar.
+        /// </summary>
+        string InputQueueAddress { get; }
+    }
+}
---
+++ src/Rebus/ISendMessagesAsync.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Rebus
+{
+    /// <summary>
+    /// Interface of something that is capable of sending a <see cref="TransportMessageToSend"/> somewhere asynchronously.
+    /// </summary>
+    public interface ISendMessagesAsync
+    {
+        /// <summary>
+        /// Asynchronously sends the specified <see cref="TransportMessageToSend"/> to the endpoint with
+        /// the specified input queue name, enlisting in the specified <see cref="ITransactionContext"/>.
+        /// </summary>
+        Task SendAsync(string destination, TransportMessageToSend message, ITransactionContext context);
+    }
+}
