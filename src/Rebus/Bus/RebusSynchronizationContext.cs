95371ac132edde572c2a521e24c151c3fdbe55d3
Asger Hallas <ahl@asgerhallas.dk>
2015-03-30 12:14:19 +0200

    @mookid8000 says captured call context is automatically restored, when posted callbacks are invoked.

--- src/Rebus/Bus/RebusSynchronizationContext.cs
+++ src/Rebus/Bus/RebusSynchronizationContext.cs
@@ -6,23 +6,20 @@ namespace Rebus.Bus
 {
     internal class RebusSynchronizationContext : SynchronizationContext
     {
-        readonly ConcurrentQueue<Tuple<SendOrPostCallback, object, ITransactionContext>> callbacks =
-            new ConcurrentQueue<Tuple<SendOrPostCallback, object, ITransactionContext>>();
+        readonly ConcurrentQueue<Tuple<SendOrPostCallback, object>> callbacks =
+            new ConcurrentQueue<Tuple<SendOrPostCallback, object>>();
 
         public override void Post(SendOrPostCallback d, object state)
         {
-            var txc = TransactionContext.Current;
-            callbacks.Enqueue(Tuple.Create(d, state, txc));
+            callbacks.Enqueue(Tuple.Create(d, state));
         }
 
         internal void Run()
         {
-            Tuple<SendOrPostCallback, object, ITransactionContext> tuple;
+            Tuple<SendOrPostCallback, object> tuple;
             while (callbacks.TryDequeue(out tuple))
             {
-                TransactionContext.Set(tuple.Item3);
                 tuple.Item1(tuple.Item2);
-                TransactionContext.Clear();
             }
         }
     }
