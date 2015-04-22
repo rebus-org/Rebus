4a51c62c4dad267142b9c60d2ab4ea2ef9e49b29
Mogens Heller Grabe <mookid8000@gmail.com>
2015-04-22 16:15:48 +0200

    nlog logging

---
+++ Rebus.NLog/NLogConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+using Rebus.Config;
+using Rebus.Logging;
+
+namespace Rebus.NLog
+{
+    public static class NLogConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures Rebus to use NLog for all of its internal logging, getting its loggers by calling logger <see cref="LogManager.GetLogger(string)"/>
+        /// </summary>
+        public static void NLog(this RebusLoggingConfigurer configurer)
+        {
+            RebusLoggerFactory.Current = new NLogLoggerFactory();
+        }
+    }
+}
