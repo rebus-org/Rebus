7cc00e81ba518b785899c247163fb69c396abe1a
Mogens Heller Grabe <mookid8000@gmail.com>
2015-04-22 21:34:16 +0200

    log4net too

---
+++ Rebus.Log4net/Log4NetConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using log4net;
+using Rebus.Config;
+using Rebus.Logging;
+
+namespace Rebus.Log4net
+{
+    public static class Log4NetConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures Rebus to use Log4Net for all of its internal logging, getting its loggers by calling logger <see cref="LogManager.GetLogger(Type)"/>
+        /// </summary>
+        public static void NLog(this RebusLoggingConfigurer configurer)
+        {
+            RebusLoggerFactory.Current = new Log4NetLoggerFactory();
+        }
+    }
+}
