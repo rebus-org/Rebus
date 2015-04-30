f154d1d1dbc5022c80fd6af458d7511846a49409
Mogens Heller Grabe <mookid8000@gmail.com>
2015-04-30 10:16:26 +0200

    clean up asb queues properly before test

---
+++ Rebus/Threading/ParallelismCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Rebus.Threading
+{
+    public class ParallelismCounter
+    {
+        readonly int _maxParallelism;
+        int _currentParallelism;
+
+        public ParallelismCounter(int maxParallelism)
+        {
+            _maxParallelism = Math.Max(1, maxParallelism);
+        }
+
+        public bool CanContinue()
+        {
+            return _currentParallelism < _maxParallelism;
+        }
+
+        public IDisposable Begin()
+        {
+            _currentParallelism++;
+            return new ParallismCounterDecreaser(() => _currentParallelism--);
+        }
+
+        class ParallismCounterDecreaser : IDisposable
+        {
+            readonly Action _disposeAction;
+
+            public ParallismCounterDecreaser(Action disposeAction)
+            {
+                _disposeAction = disposeAction;
+            }
+
+            public void Dispose()
+            {
+                _disposeAction();
+            }
+        }
+    }
+}
