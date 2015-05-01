11252a99cad8785d53ba90bab18dc3ce37000dec
Mogens Heller Grabe <mookid8000@gmail.com>
2015-05-01 22:06:29 +0200

    ninject container adapter

---
+++ Rebus.Ninject.Tests/NinjectContainerAdapterTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Activation;
+
+namespace Rebus.Ninject.Tests
+{
+    [TestFixture]
+    public class NinjectContainerAdapterTests : ContainerTests<NinjectContainerAdapterFactory>
+    {
+    }
+}
