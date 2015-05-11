45dd89c73377528df436e8f13cf97d01b92391ad
Mogens Heller Grabe <mookid8000@gmail.com>
2015-05-11 10:12:34 +0200

    use the defaults

--- Rebus/Retry/Simple/SimpleRetryStrageConfigurationExtensions.cs
+++ Rebus/Retry/Simple/SimpleRetryStrageConfigurationExtensions.cs
@@ -2,9 +2,17 @@
 
 namespace Rebus.Retry.Simple
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class SimpleRetryStrageConfigurationExtensions
     {
-        public static void SimpleRetryStrategy(this OptionsConfigurer optionsConfigurer, string errorQueueAddress = null, int maxDeliveryAttempts = 5)
+        /// <summary>
+        /// Configures the simple retry strategy, using the specified error queue address and number of delivery attempts
+        /// </summary>
+        public static void SimpleRetryStrategy(this OptionsConfigurer optionsConfigurer,
+            string errorQueueAddress = SimpleRetryStrategySettings.DefaultErrorQueueName,
+            int maxDeliveryAttempts = SimpleRetryStrategySettings.DefaultNumberOfDeliveryAttempts)
         {
             optionsConfigurer.Register(c => new SimpleRetryStrategySettings(errorQueueAddress, maxDeliveryAttempts));
         }
