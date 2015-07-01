ee6b3df2cecc710a153ae363025a0315d02d9e56
Henrik Drachmann <hdr@d60.dk>
2015-07-01 17:23:35 +0200

    removed tablestorage from branch

--- Rebus.AzureTableStorage.Tests/AzureTableStorageBasicSendReceive.cs
+++
@@ -1,8 +0,0 @@
-﻿using NUnit.Framework;
-using Rebus.Tests.Contracts.Transports;
-
-namespace Rebus.AzureTableStorage.Tests
-{
-    [TestFixture]
-    public class AzureTableStorageBasicSendReceive : BasicSendReceive<AzureTableStorageTransportFactory> { }
-}
--- Rebus.AzureTableStorage.Tests/AzureTableStorageMessageExpiration.cs
+++
@@ -1,8 +0,0 @@
-﻿using NUnit.Framework;
-using Rebus.Tests.Contracts.Transports;
-
-namespace Rebus.AzureTableStorage.Tests
-{
-    [TestFixture]
-    public class AzureTableStorageMessageExpiration : MessageExpiration<AzureTableStorageTransportFactory> { }
-}
