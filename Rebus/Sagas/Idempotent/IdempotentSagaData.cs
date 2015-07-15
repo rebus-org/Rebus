bdf78298fae3ffe3bd38cd6dbc05578451eb6664
Mogens Heller Grabe <mookid8000@gmail.com>
2015-07-15 23:07:08 +0200

    finish up!

--- Rebus/Sagas/Idempotent/IdempotentSagaData.cs
+++ Rebus/Sagas/Idempotent/IdempotentSagaData.cs
@@ -1,14 +1,10 @@
-﻿using System;
-
-namespace Rebus.Sagas.Idempotent
+﻿namespace Rebus.Sagas.Idempotent
 {
     /// <summary>
     /// Convenient standard implementation of <see cref="IIdempotentSagaData"/>
     /// </summary>
-    public abstract class IdempotentSagaData : IIdempotentSagaData
+    public abstract class IdempotentSagaData : SagaData, IIdempotentSagaData
     {
-        public Guid Id { get; set; }
-        public int Revision { get; set; }
         public IdempotencyData IdempotencyData { get; set; }
     }
 }
---
+++ Rebus/Sagas/SagaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rebus.Sagas
+{
+    /// <summary>
+    /// Convenient implementation of <see cref="ISagaData"/>
+    /// </summary>
+    public abstract class SagaData : ISagaData
+    {
+        public Guid Id { get; set; }
+        public int Revision { get; set; }
+    }
+}
