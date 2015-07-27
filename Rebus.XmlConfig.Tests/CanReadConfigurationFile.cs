93d4a9587ce5ec41dcb3c34d59b700b5583db863
Mogens Heller Grabe <mookid8000@gmail.com>
2015-07-27 09:37:46 +0200

    added XML-based endpoint mappings

---
+++ Rebus.XmlConfig.Tests/CanReadConfigurationFile.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Tests;
+using Rebus.Transport.InMem;
+
+namespace Rebus.XmlConfig.Tests
+{
+    [TestFixture]
+    public class CanReadConfigurationFile : FixtureBase
+    {
+        [Test]
+        public void ItWorks()
+        {
+            using (AppConfig.Change("Examples/App-01.config"))
+            {
+                Configure.With(new BuiltinHandlerActivator())
+                    .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "bimse"))
+                    .Routing(r => r.TypeBasedRoutingFromAppConfig())
+                    .Start();
+            }
+        }
+    }
+
+    public class SomeExistingType { }
+}
---
+++ Rebus.XmlConfig/EndpointConfigurationElement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace Rebus.XmlConfig
+{
+    /// <summary>
+    /// Contains the configured endpoint mappings
+    /// </summary>
+    public class EndpointConfigurationElement : ConfigurationElementCollection, IEnumerable<EndpointMapping>
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new EndpointMapping();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((EndpointMapping)element).Messages;
+        }
+
+        /// <summary>
+        /// Performs some redundant action because that's just how .NET works
+        /// </summary>
+        public new IEnumerator<EndpointMapping> GetEnumerator()
+        {
+            for (var index = 0; index < Count; index++)
+            {
+                yield return (EndpointMapping)BaseGet(index);
+            }
+        }
+    }
+}
