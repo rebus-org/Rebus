f8b7d5f629a1c1ac5274cce18ee0c426c6dd5933
Mogens Heller Grabe <mookid8000@gmail.com>
2015-07-28 13:20:36 +0200

    yet another extension

--- Rebus/Extensions/BusExtensions.cs
+++ Rebus/Extensions/BusExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Rebus.Bus;
 
@@ -41,6 +42,24 @@ namespace Rebus.Extensions
             var topic = typeof(TEvent).AssemblyQualifiedName;
 
             return bus.Unsubscribe(topic);
+        }
+
+        /// <summary>
+        /// Publishes the event message on the topic defined by the assembly-qualified name of <typeparamref name="TEvent"/>. 
+        /// While this kind of pub/sub can work universally with the general topic-based routing, it works especially well with type-based routing,
+        /// which can be enabled by going 
+        /// <code>
+        /// Configure.With(...)
+        ///     .(...)
+        ///     .Routing(r => r.TypeBased()
+        ///             .Map&lt;SomeMessage&gt;("someEndpoint")
+        ///             .(...))
+        /// </code>
+        /// in the configuration
+        /// </summary>
+        public static Task Publish<TEvent>(this IBus bus, TEvent eventMessage, Dictionary<string, string> optionalHeaders = null)
+        {
+            return bus.Publish(typeof (TEvent).AssemblyQualifiedName, eventMessage, optionalHeaders);
         }
     }
 }
