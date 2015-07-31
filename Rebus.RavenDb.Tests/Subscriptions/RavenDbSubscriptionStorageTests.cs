8c5839538988b4be258432be4169ce3b0b989045
Martin Humlund Clausen <mhc@d60.dk>
2015-08-01 00:18:06 +0200

    Added RavenDbSubscriptionsStorage

---
+++ Rebus.RavenDb.Tests/Subscriptions/RavenDbSubscriptionStorageTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Raven.Client.Embedded;
+using Rebus.RavenDb.Subscriptions;
+using Rebus.Subscriptions;
+using Rebus.Tests.Contracts.Subscriptions;
+
+namespace Rebus.RavenDb.Tests.Subscriptions
+{
+    [TestFixture]
+    public class RavenDbSubscriptionStorageTests : BasicSubscriptionOperations<RavenDbSubscriptionStorageFactory>
+    {
+    }
+
+    public class RavenDbSubscriptionStorageFactory : ISubscriptionStorageFactory
+    {
+        private EmbeddableDocumentStore _documentStore;
+
+        public ISubscriptionStorage Create()
+        {
+            _documentStore = new EmbeddableDocumentStore()
+           {
+               RunInMemory = true,
+           };
+
+            _documentStore.Configuration.Storage.Voron.AllowOn32Bits = true;
+
+            _documentStore.Initialize();
+
+            return new RavenDbSubscriptionStorage(_documentStore);
+
+        }
+
+        public void Cleanup()
+        {
+            _documentStore.Dispose();
+            _documentStore = null;
+        }
+    }
+}
---
+++ Rebus.RavenDb/Subscriptions/RavenDbSubscriptionStorage.cs
@@ -0,0 +1,67 @@
+using System.Threading.Tasks;
+using Raven.Client;
+using Rebus.Subscriptions;
+
+namespace Rebus.RavenDb.Subscriptions
+{
+    public class RavenDbSubscriptionStorage : ISubscriptionStorage
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public RavenDbSubscriptionStorage(IDocumentStore documentStore)
+        {
+            _documentStore = documentStore;
+        }
+
+        public async Task<string[]> GetSubscriberAddresses(string topic)
+        {
+            using (var session = _documentStore.OpenAsyncSession())
+            {
+                await EnsureSubscriptionsExists(session);
+
+                var subscriptions = await session.LoadAsync<Subscription>(Subscription.Id);
+
+                return await Task.FromResult(subscriptions.GetSubscriberAddresses(topic));
+            }
+        }
+
+        public async Task RegisterSubscriber(string topic, string subscriberAddress)
+        {
+            using (var session = _documentStore.OpenAsyncSession())
+            {
+                await EnsureSubscriptionsExists(session);
+
+                var subscriptions = await session.LoadAsync<Subscription>(Subscription.Id);
+                subscriptions.RegisterSubscriber(topic, subscriberAddress);
+
+                await session.SaveChangesAsync();
+            }
+        }
+
+        public async Task UnregisterSubscriber(string topic, string subscriberAddress)
+        {
+            using (var session = _documentStore.OpenAsyncSession())
+            {
+                await EnsureSubscriptionsExists(session);
+
+                var subscriptions = await session.LoadAsync<Subscription>(Subscription.Id);
+                subscriptions.UnregisterSubscriber(topic, subscriberAddress);
+
+                await session.SaveChangesAsync();
+            }
+        }
+
+        private async Task EnsureSubscriptionsExists(IAsyncDocumentSession session)
+        {
+            var subscription = await session.LoadAsync<Subscription>(Subscription.Id);
+            if (subscription == null)
+            {
+                subscription = new Subscription();
+                await session.StoreAsync(subscription);
+                await session.SaveChangesAsync();
+            }
+        }
+
+        public bool IsCentralized { get; private set; }
+    }
+}
