a63c250be3ea7534eda4a1543212e521fbd97656
Mogens Heller Grabe <mookid8000@gmail.com>
2015-08-10 22:33:57 +0200

    comments, baby!

--- Rebus/Pipeline/Send/AutoHeadersOutgoingStep.cs
+++ Rebus/Pipeline/Send/AutoHeadersOutgoingStep.cs
@@ -7,10 +7,20 @@ using Rebus.Messages;
 
 namespace Rebus.Pipeline.Send
 {
+    /// <summary>
+    /// Outgoing step that picks up <see cref="HeaderAttribute"/> from the message type, automatically adding headers
+    /// with <see cref="HeaderAttribute.Key"/> set to <see cref="HeaderAttribute.Value"/> if a header with such key has not already been added.
+    /// </summary>
+    [StepDocumentation(@"If the outgoing message type has [HeaderAttribute(..., ...)] on it, the found headers will automatically be picked up and added to the outgoing message.
+
+Headers already on the message will not be overwritten.")]
     public class AutoHeadersOutgoingStep : IOutgoingStep
     {
         readonly ConcurrentDictionary<Type, IEnumerable<HeaderAttribute>> _headersToAssign = new ConcurrentDictionary<Type, IEnumerable<HeaderAttribute>>();
 
+        /// <summary>
+        /// Carries out the auto-header logic
+        /// </summary>
         public async Task Process(OutgoingStepContext context, Func<Task> next)
         {
             var message = context.Load<Message>();
