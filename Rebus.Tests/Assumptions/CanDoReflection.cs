d872842103a7ab976de533a496e6015b11527839
Mogens Heller Grabe <mookid8000@gmail.com>
2015-09-24 14:05:01 +0200

    added reflection test

---
+++ Rebus.Tests/Assumptions/CanDoReflection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using Rebus.Bus;
+using Rebus.Extensions;
+using Rebus.Handlers;
+
+namespace Rebus.Tests.Assumptions
+{
+    [TestFixture]
+    public class CanDoReflection
+    {
+        [Test, Ignore("played around with assembly-scanning")]
+        public void YeahItWorks()
+        {
+            IBus bus = GetBus();
+
+            var assemblyToScan = Assembly.GetExecutingAssembly();
+
+            var handledMessageTypes = assemblyToScan
+                .GetTypes()
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof (IHandleMessages<>))
+                    .Select(i => i.GetGenericArguments().Single()))
+                .Distinct()
+                .ToList();
+
+            foreach (var messageType in handledMessageTypes)
+            {
+                bus.Advanced.Topics.Subscribe(messageType.GetSimpleAssemblyQualifiedName());
+            }
+        }
+
+        IBus GetBus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
