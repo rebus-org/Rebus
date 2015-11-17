a70c5c5741453a701f828935be7fa5cd95c3fbb0
Andrew Watson <a.watson@puzsol.com.au>
2015-11-17 14:08:59 +1100

    LightInject 4.0.0
    Adding LightInject as an available IoC for rebus

---
+++ Rebus.LightInject.Tests/LightInjectContainerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Tests.Contracts.Activation;
+
+namespace Rebus.LightInject.Tests
+{
+    [TestFixture]
+    public class LightInjectContainerTests : ContainerTests<LightInjectContainerAdapterFactory>
+    {
+    }
+}
---
+++ Rebus.LightInject/LightInjectContainerAdapter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LightInject;
+using Rebus.Activation;
+using Rebus.Bus;
+using Rebus.Extensions;
+using Rebus.Handlers;
+using Rebus.Pipeline;
+using Rebus.Transport;
+#pragma warning disable 1998
+
+namespace Rebus.LightInject
+{
+    public class LightInjectContainerAdapter : IContainerAdapter
+    {
+        readonly IServiceContainer _serviceContainer;
+
+        /// <summary>
+        /// Constructs the adapter, using the specified container
+        /// </summary>
+        public LightInjectContainerAdapter(IServiceContainer container)
+        {
+            _serviceContainer = container;
+
+            _serviceContainer.Register<IMessageContext>(factory => getCurrentMessageContext());
+        }
+
+        /// <summary>
+        /// Resolves all handlers for the given <typeparamref name="TMessage"/> message type
+        /// </summary>
+        public async Task<IEnumerable<IHandleMessages<TMessage>>> GetHandlers<TMessage>(TMessage message, ITransactionContext transactionContext)
+        {
+            var handlerInstances = _serviceContainer
+                .GetAllInstances<IHandleMessages<TMessage>>()
+                .ToList();
+
+            transactionContext.OnDisposed(() =>
+            {
+                handlerInstances
+                    .OfType<IDisposable>()
+                    .ForEach(disposable =>
+                    {
+                        disposable.Dispose();
+                    });
+            });
+
+            return handlerInstances;
+        }
+
+        /// <summary>
+        /// Stores the bus instance
+        /// </summary>
+        public void SetBus(IBus bus)
+        {
+            _serviceContainer.Register<IBus>(factory => bus, new PerContainerLifetime());
+
+            // Force the per container lifetime to get the reference to the bus object so it will be disposed when the container is disposed
+            _serviceContainer.GetInstance<IBus>();
+        }
+
+        /// <summary>
+        /// Returns the current message context and ensures it is not null
+        /// </summary>
+        /// <returns>IMessageContext</returns>
+        private IMessageContext getCurrentMessageContext()
+        {
+            var currentMessageContext = MessageContext.Current;
+            if (currentMessageContext == null)
+            {
+                throw new InvalidOperationException("Attempted to inject the current message context from MessageContext.Current, but it was null! Did you attempt to resolve IMessageContext from outside of a Rebus message handler?");
+            }
+            return currentMessageContext;
+        }
+    }
+}
