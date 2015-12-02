6ed6b0bcb6e39775b340801abcd0975a54e455ba
Mogens Heller Grabe <mookid8000@gmail.com>
2015-12-02 19:26:05 +0100

    did some SQS cuddling

---
+++ Rebus.AmazonSQS.Tests/Bimse.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using NUnit.Framework;
+
+namespace Rebus.AmazonSQS.Tests
+{
+    [TestFixture]
+    public class Bimse
+    {
+        [Test]
+        public async Task NizzleName()
+        {
+            var credentials = new BasicAWSCredentials("AKIAI6CUQBQYYT7COZ3Q", "m8bkPaoJ6CGia9GPQ6LjjwlE9eNuwpRynZtaErdx");
+            var client = new AmazonSQSClient(credentials, new AmazonSQSConfig {ServiceURL = "https://sqs.eu-central-1.amazonaws.com" });
+
+            await client.CreateQueueAsync(new CreateQueueRequest("myqueue"));
+        }
+    }
+}
---
+++ Rebus.Tests/Assumptions/TestConcurrentDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Rebus.Tests.Assumptions
+{
+    [TestFixture]
+    public class TestConcurrentDictionary
+    {
+        [Test]
+        public void NizzleName()
+        {
+            var dictionary = new ConcurrentDictionary<string, string>();
+
+            ThreadPool.QueueUserWorkItem(__ =>
+            {
+                dictionary.GetOrAdd("bimse", _ =>
+                {
+                    Console.WriteLine("waiting");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("done");
+                    return "hej";
+                });
+            });
+
+            ThreadPool.QueueUserWorkItem(__ =>
+            {
+                dictionary.GetOrAdd("bimse", _ =>
+                {
+                    Console.WriteLine("waiting");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("done");
+                    return "hej";
+                });
+            });
+
+            Thread.Sleep(3000);
+        }
+    }
+}
