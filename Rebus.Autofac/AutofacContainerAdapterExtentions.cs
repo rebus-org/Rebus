528d821dba2cea7826d41dda68aa96fb992f62ca
Gert Jansen van Rensburg <gertjvr@gmail.com>
2016-01-21 20:42:02 +1000

    Registering IBus as a keyed service with AutofacContainerAdapter. Added away to register IBus without updating the container.

---
+++ Rebus.Autofac/AutofacContainerAdapterExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Autofac;
+using Rebus.Pipeline;
+
+namespace Rebus.Autofac
+{
+    public static class AutofacContainerAdapterExtentions
+    {
+        public static void RegisterRebus(this ContainerBuilder self)
+        {
+            self
+                .Register(c =>
+                {
+                    var currentMessageContext = MessageContext.Current;
+                    if (currentMessageContext == null)
+                    {
+                        throw new InvalidOperationException("Attempted to inject the current message context from MessageContext.Current, but it was null! Did you attempt to resolve IMessageContext from outside of a Rebus message handler?");
+                    }
+                    return currentMessageContext;
+                })
+                .InstancePerDependency()
+                .ExternallyOwned();
+        }
+    }
+}
