06bcb91154174baa22b6cb20a388c187e7a0a3e8
Mogens Heller Grabe <mookid8000@gmail.com>
2016-01-24 16:12:05 +0100

    started adding ability to host an OWIN endpoint

---
+++ Rebus.Owin.Tests/SimpleGetRequest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Tests;
+using Rebus.Transport.InMem;
+
+namespace Rebus.Owin.Tests
+{
+    [TestFixture]
+    public class SimpleGetRequest : FixtureBase
+    {
+        BuiltinHandlerActivator _activator;
+
+        protected override void SetUp()
+        {
+            _activator = new BuiltinHandlerActivator();
+
+            Configure.With(_activator)
+                .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "doesn't matter"))
+                .Options(o =>
+                {
+                    o.AddWebHost("http://localhost:9090", app =>
+                    {
+
+                    });
+                })
+                .Start();
+        }
+
+        [Test]
+        public void CanProcessIt()
+        {
+            
+        }
+    }
+}
---
+++ Rebus.Owin/OwinOptionsConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Owin;
+using Rebus.Bus;
+using Rebus.Config;
+
+namespace Rebus.Owin
+{
+    public static class OwinOptionsConfigurationExtensions
+    {
+        public static void AddWebHost(this OptionsConfigurer configurer, string listenUrl, Action<IAppBuilder> action)
+        {
+            configurer.Decorate(c =>
+            {
+                var bus = c.Get<IBus>();
+
+                return bus;
+            });
+        }
+    }
+
+    class RebusWebHost
+    {
+        public RebusWebHost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
