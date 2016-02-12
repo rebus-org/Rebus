d759b31bd8e8eb3382451ec5d175331d21ddf90e
Mogens Heller Grabe <mookid8000@gmail.com>
2016-02-12 12:32:27 +0100

    alt+enter

--- Rebus/Sagas/Saga.cs
+++ Rebus/Sagas/Saga.cs
@@ -151,7 +151,7 @@ namespace Rebus.Sagas
                     }
                     catch (Exception exception)
                     {
-                        throw new RebusApplicationException(exception, string.Format("Could not extract correlation value from message {0}", typeof(TMessage)));
+                        throw new RebusApplicationException(exception, $"Could not extract correlation value from message {typeof (TMessage)}");
                     }
                 };
 
