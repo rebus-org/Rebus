b4da447fee8d1940169903d45cb0204d2ca7f0a4
Mogens Heller Grabe <mookid8000@gmail.com>
2016-05-09 14:08:19 +0200

    added rabbitmq/explicit routing scenario

---
+++ Rebus.RabbitMq.Tests/TestRabbitMqAndExplicitRouting.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Bus;
+using Rebus.Config;
+using Rebus.Logging;
+using Rebus.Tests;
+using Rebus.Tests.Extensions;
+
+#pragma warning disable 1998
+
+namespace Rebus.RabbitMq.Tests
+{
+    [TestFixture]
+    public class TestRabbitMqAndExplicitRouting : FixtureBase
+    {
+        const string ConnectionString = RabbitMqTransportFactory.ConnectionString;
+
+        readonly string _queueName = TestConfig.QueueName("manual_routing");
+
+        BuiltinHandlerActivator _activator;
+        IBus _bus;
+
+        protected override void SetUp()
+        {
+            _activator = Using(new BuiltinHandlerActivator());
+
+            Configure.With(_activator)
+                .Logging(l => l.Console(minLevel:LogLevel.Warn))
+                .Transport(t => t.UseRabbitMq(ConnectionString, _queueName))
+                .Start();
+
+            var client = Using(new BuiltinHandlerActivator());
+
+            _bus = Configure.With(client)
+                .Logging(l => l.Console(minLevel:LogLevel.Warn))
+                .Transport(t => t.UseRabbitMqAsOneWayClient(ConnectionString))
+                .Start();
+        }
+
+        [Test]
+        public async Task ReceivesManuallyRoutedMessage()
+        {
+            var gotTheMessage = new ManualResetEvent(false);
+
+            _activator.Handle<string>(async str =>
+            {
+                gotTheMessage.Set();
+            });
+
+            Console.WriteLine($"Sending 'hej med dig min ven!' message to '{_queueName}'");
+
+            await _bus.Advanced.Routing.Send(_queueName, "hej med dig min ven!");
+
+            Console.WriteLine("Waiting for message to arrive");
+
+            gotTheMessage.WaitOrDie(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine("Got it :)");
+        }
+    }
+}
