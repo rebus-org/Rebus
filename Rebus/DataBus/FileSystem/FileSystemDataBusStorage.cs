e0f60ef730433d0ec7a6fb19139bfc298128ff3a
Mogens Heller Grabe <mookid8000@gmail.com>
2016-05-20 19:13:31 +0200

    configuration API for SQL Server

---
+++ Rebus/DataBus/FileSystem/FileSystemDataBusStorage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Rebus.Bus;
+using Rebus.Logging;
+
+namespace Rebus.DataBus.FileSystem
+{
+    public class FileSystemDataBusStorage : IDataBusStorage, IInitializable
+    {
+        readonly string _directoryPath;
+        readonly ILog _log;
+
+        public FileSystemDataBusStorage(string directoryPath, IRebusLoggerFactory rebusLoggerFactory)
+        {
+            if (directoryPath == null) throw new ArgumentNullException(nameof(directoryPath));
+            if (rebusLoggerFactory == null) throw new ArgumentNullException(nameof(rebusLoggerFactory));
+            _directoryPath = directoryPath;
+            _log = rebusLoggerFactory.GetCurrentClassLogger();
+        }
+
+        public void Initialize()
+        {
+            if (!Directory.Exists(_directoryPath))
+            {
+                _log.Info("Creating directory {0}", _directoryPath);
+                Directory.CreateDirectory(_directoryPath);
+            }
+        }
+
+        public Task Save(string id, Stream source)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Stream Read(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Rebus/DataBus/SqlServer/SqlServerDataBusConfigurationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Rebus.Config;
+using Rebus.Logging;
+using Rebus.Persistence.SqlServer;
+
+namespace Rebus.DataBus.SqlServer
+{
+    /// <summary>
+    /// Configuration extensions for SQL Server data bus
+    /// </summary>
+    public static class SqlServerDataBusConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures the data bus to store data in a central SQL Server 
+        /// </summary>
+        public static void StoreInSqlServer(this StandardConfigurer<IDataBusStorage> configurer, string connectionStringOrConnectionStringName, string tableName, bool automaticallyCreateTables = true)
+        {
+            if (configurer == null) throw new ArgumentNullException(nameof(configurer));
+            if (connectionStringOrConnectionStringName == null) throw new ArgumentNullException(nameof(connectionStringOrConnectionStringName));
+            if (tableName == null) throw new ArgumentNullException(nameof(tableName));
+
+            configurer.Register(c =>
+            {
+                var loggerFactory = c.Get<IRebusLoggerFactory>();
+                var connectionProvider = new DbConnectionProvider(connectionStringOrConnectionStringName, loggerFactory);
+                return new SqlServerDataBusStorage(connectionProvider, tableName, automaticallyCreateTables, loggerFactory);
+            });
+        }
+    }
+}
