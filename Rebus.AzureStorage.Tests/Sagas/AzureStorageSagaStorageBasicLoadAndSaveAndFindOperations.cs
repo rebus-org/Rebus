7c8cf419372bb840ed8ca61b5773127d87157598
Jeffrey Becker <jabecker@jmt.com>
2016-06-22 13:19:20 -0400

    finished unit tests

--- Rebus.AzureStorage.Tests/Sagas/AzureStorageSagaStorageBasicLoadAndSaveAndFindOperations.cs
+++ Rebus.AzureStorage.Tests/Sagas/AzureStorageSagaStorageBasicLoadAndSaveAndFindOperations.cs
@@ -1,16 +1,16 @@
 using NUnit.Framework;
+using Rebus.Tests;
 using Rebus.Tests.Contracts.Sagas;
+using Rebus.Tests.Persistence.SqlServer;
 
 namespace Rebus.AzureStorage.Tests.Sagas
 {
-    public class AzureStorageSagaStorageBasicLoadAndSaveAndFindOperations :
-        BasicLoadAndSaveAndFindOperations<AzureStorageSagaStorageFactory>
-    {
+    [TestFixture, Category(TestCategory.Azure)]
+    public class AzureStorageSagaStorageBasicLoadAndSaveAndFindOperations : BasicLoadAndSaveAndFindOperations<AzureStorageSagaStorageFactory> { }
 
-        [TestFixtureSetUp]
-        public void SetupFixture()
-        {
-            //AzureStorageSagaStorageFactory.DropAndRecreateObjects();
-        }
-    }
+    [TestFixture, Category(TestCategory.Azure)]
+    public class AzureStoragerSagaStorageConcurrencyHandling : ConcurrencyHandling<SqlServerSagaStorageFactory> { }
+
+    [TestFixture, Category(TestCategory.Azure)]
+    public class AzureStorageSagaStorageSagaIntegrationTests : SagaIntegrationTests<SqlServerSagaStorageFactory> { }
 }
