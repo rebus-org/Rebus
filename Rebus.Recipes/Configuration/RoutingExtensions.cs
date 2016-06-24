70ba5f85673c557321238875851b678ca7012ea9
Jeffrey Becker <jabecker@jmt.com>
2016-06-24 08:41:18 -0400

    routing syntax sugar

---
+++ Rebus.Recipes/Configuration/RoutingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rebus.Config;
+using Rebus.Routing;
+using Rebus.Routing.TypeBased;
+
+namespace Rebus.Recipes.Configuration
+{
+    public static class RoutingExtensions
+    {
+        public static void SendAllMessagesTo(this StandardConfigurer<IRouter> routeConfigurer, string queueName)
+        {
+            routeConfigurer.TypeBased().MapFallback(queueName);
+        }
+    }
+}
