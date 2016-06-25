80a95026dc70778db5c7a9d937cf841a7a46ab7a
Jeffrey Becker <jabecker@jmt.com>
2016-06-25 12:29:45 -0400

    fix bug with azure transports blowing up

--- Rebus.AzureStorage.Tests/Transport/AzureStorageQueuesTransportBasicSendReceive.cs
+++ Rebus.AzureStorage.Tests/Transport/AzureStorageQueuesTransportBasicSendReceive.cs
@@ -1,8 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.AzureStorage.Transport;
+using Rebus.Messages;
+using Rebus.Tests;
 using Rebus.Tests.Contracts.Transports;
+using Rebus.Time;
 
 namespace Rebus.AzureStorage.Tests.Transport
 {
     [TestFixture, Category(TestCategory.Azure)]
-    public class AzureStorageQueuesTransportBasicSendReceive : BasicSendReceive<AzureStorageQueuesTransportFactory> { }
+    public class AzureStorageQueuesTransportBasicSendReceive : BasicSendReceive<AzureStorageQueuesTransportFactory>
+    {
+        [Test]
+        public async Task GetQueueVisibilityDelayOrNull_NeverReturnsNegativeTimespans()
+        {
+            RebusTimeMachine.FakeIt(DateTimeOffset.Parse("2016-06-25T12:20:52.6038001-04:00"));
+            var result = AzureStorageQueuesTransport.GetQueueVisibilityDelayOrNull(new Dictionary<string, string>
+            {
+                {Headers.DeferredUntil, "2016-06-25T12:00:00.6038001-04:00"}
+            });
+            RebusTimeMachine.Reset();
+            Assert.Null(result);
+
+        }
+        [Test]
+        public async Task GetQueueVisibilityDelayOrNull_StillReturnsPositiveTimespans()
+        {
+            RebusTimeMachine.FakeIt(DateTimeOffset.Parse("2016-06-25T12:00:00.6038001-04:00"));
+            var result = AzureStorageQueuesTransport.GetQueueVisibilityDelayOrNull(new Dictionary<string, string>
+            {
+                {Headers.DeferredUntil, "2016-06-25T12:20:52.6038001-04:00"}
+            });
+            RebusTimeMachine.Reset();
+            Assert.Greater(result, TimeSpan.Zero);
+
+        }
+    }
 }
