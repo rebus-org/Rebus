61f9df25f5b90f680d478bdd6cda71c5eeaf041d
Mogens Heller Grabe <mookid8000@gmail.com>
2016-06-27 23:08:05 +0200

    added some more SQL Server config overloads

--- Rebus/Transport/SqlServer/SqlServerTransportConfigurationExtensions.cs
+++ Rebus/Transport/SqlServer/SqlServerTransportConfigurationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Rebus.Config;
+﻿using System;
+using System.Threading.Tasks;
+using Rebus.Config;
 using Rebus.Logging;
 using Rebus.Persistence.SqlServer;
 using Rebus.Pipeline;
@@ -18,9 +20,21 @@ namespace Rebus.Transport.SqlServer
         /// The table specified by <paramref name="tableName"/> will be used to store messages.
         /// The message table will automatically be created if it does not exist.
         /// </summary>
+        public static void UseSqlServerAsOneWayClient(this StandardConfigurer<ITransport> configurer, Func<Task<IDbConnection>> connectionFactory, string tableName)
+        {
+            Configure(configurer, loggerFactory => new DbConnectionFactoryProvider(connectionFactory, loggerFactory), tableName, null);
+
+            OneWayClientBackdoor.ConfigureOneWayClient(configurer);
+        }
+
+        /// <summary>
+        /// Configures Rebus to use SQL Server to transport messages as a one-way client (i.e. will not be able to receive any messages).
+        /// The table specified by <paramref name="tableName"/> will be used to store messages.
+        /// The message table will automatically be created if it does not exist.
+        /// </summary>
         public static void UseSqlServerAsOneWayClient(this StandardConfigurer<ITransport> configurer, string connectionStringOrConnectionStringName, string tableName)
         {
-            Configure(configurer, connectionStringOrConnectionStringName, tableName, null);
+            Configure(configurer, loggerFactory => new DbConnectionProvider(connectionStringOrConnectionStringName, loggerFactory), tableName, null);
 
             OneWayClientBackdoor.ConfigureOneWayClient(configurer);
         }
@@ -30,18 +44,28 @@ namespace Rebus.Transport.SqlServer
         /// store messages, and the "queue" specified by <paramref name="inputQueueName"/> will be used when querying for messages.
         /// The message table will automatically be created if it does not exist.
         /// </summary>
-        public static void UseSqlServer(this StandardConfigurer<ITransport> configurer, string connectionStringOrConnectionStringName, string tableName, string inputQueueName)
+        public static void UseSqlServer(this StandardConfigurer<ITransport> configurer, Func<Task<IDbConnection>> connectionFactory, string tableName, string inputQueueName)
         {
-            Configure(configurer, connectionStringOrConnectionStringName, tableName, inputQueueName);
+            Configure(configurer, loggerFactory => new DbConnectionFactoryProvider(connectionFactory, loggerFactory), tableName, inputQueueName);
         }
 
-        static void Configure(StandardConfigurer<ITransport> configurer, string connectionString, string tableName, string inputQueueName)
+        /// <summary>
+        /// Configures Rebus to use SQL Server as its transport. The table specified by <paramref name="tableName"/> will be used to
+        /// store messages, and the "queue" specified by <paramref name="inputQueueName"/> will be used when querying for messages.
+        /// The message table will automatically be created if it does not exist.
+        /// </summary>
+        public static void UseSqlServer(this StandardConfigurer<ITransport> configurer, string connectionStringOrConnectionStringName, string tableName, string inputQueueName)
+        {
+            Configure(configurer, loggerFactory => new DbConnectionProvider(connectionStringOrConnectionStringName, loggerFactory), tableName, inputQueueName);
+        }
+
+        static void Configure(StandardConfigurer<ITransport> configurer, Func<IRebusLoggerFactory, IDbConnectionProvider> connectionProviderFactory, string tableName, string inputQueueName)
         {
             configurer.Register(context =>
             {
                 var rebusLoggerFactory = context.Get<IRebusLoggerFactory>();
                 var asyncTaskFactory = context.Get<IAsyncTaskFactory>();
-                var connectionProvider = new DbConnectionProvider(connectionString, rebusLoggerFactory);
+                var connectionProvider = connectionProviderFactory(rebusLoggerFactory);
                 var transport = new SqlServerTransport(connectionProvider, tableName, inputQueueName, rebusLoggerFactory, asyncTaskFactory);
                 transport.EnsureTableIsCreated();
                 return transport;
@@ -54,7 +78,7 @@ namespace Rebus.Transport.SqlServer
                 var pipeline = c.Get<IPipeline>();
 
                 return new PipelineStepRemover(pipeline)
-                    .RemoveIncomingStep(s => s.GetType() == typeof (HandleDeferredMessagesStep));
+                    .RemoveIncomingStep(s => s.GetType() == typeof(HandleDeferredMessagesStep));
             });
         }
     }
