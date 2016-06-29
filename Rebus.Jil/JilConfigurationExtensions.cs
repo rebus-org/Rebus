9585a82c3fbb81a19be13cecbeaf64f014df14ac
Mogens Heller Grabe <mookid8000@gmail.com>
2016-06-29 09:33:52 +0200

    nuspec and config extension

--- Rebus.Jil/JilConfigurationExtensions.cs
+++ Rebus.Jil/JilConfigurationExtensions.cs
@@ -6,7 +6,7 @@ namespace Rebus.Jil
     /// <summary>
     /// Configuration extensions for the Jil serializer
     /// </summary>
-    public static class JilConfigurationExtenctions
+    public static class JilConfigurationExtensions
     {
         /// <summary>
         /// Configures Rebus to use the simple and extremely fast Jil JSON serializer
