b0210f308d1f6748d97f18c465f3effd628139ba
Enos Recanati <recanatie@hotmail.it>
2016-08-25 10:05:53 +0200

    Added ASP.NET Core ServiceProvider ContainerAdapter and Tests

---
+++ Rebus.ServiceProvider.Tests/NetCoreServiceCollectionContainerAdapterFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Rebus.Activation;
+using Rebus.Bus;
+using Rebus.Extensions;
+using Rebus.Handlers;
+using Rebus.Tests.Contracts.Activation;
+
+namespace Rebus.ServiceProvider.Tests
+{
+    public class NetCoreServiceCollectionContainerAdapterFactory : IContainerAdapterFactory
+    {
+        readonly IServiceCollection _serviceCollection = new ServiceCollection();
+
+        public void CleanUp()
+        {
+            _serviceCollection.Clear();
+        }
+
+        public IHandlerActivator GetActivator()
+        {
+            return new NetCoreServiceCollectionContainerAdapter(_serviceCollection);
+        }
+
+        public IBus GetBus()
+        {
+            var container = _serviceCollection.BuildServiceProvider();
+
+            return container.GetService<IBus>();
+        }
+
+        void IContainerAdapterFactory.RegisterHandlerType<THandler>()
+        {
+            GetHandlerInterfaces(typeof(THandler))
+                .ForEach(i => _serviceCollection.AddTransient(i, typeof(THandler)));
+        }
+
+        Type[] GetHandlerInterfaces(Type type)
+        {
+            return type.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IHandleMessages<>))
+                .ToArray();
+        }
+    }
+}
---
+++ Rebus.ServiceProvider/NetCoreServiceCollectionContainerAdapter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Rebus.Activation;
+using Rebus.Bus;
+using Rebus.Extensions;
+using Rebus.Handlers;
+using Rebus.Pipeline;
+using Rebus.Transport;
+
+namespace Rebus.ServiceProvider
+{
+    /// <summary>
+    /// Implementation of <see cref="IContainerAdapter"/> that is backed by a ServiceProvider
+    /// </summary>
+    /// <seealso cref="Rebus.Activation.IContainerAdapter" />
+    public class NetCoreServiceCollectionContainerAdapter : IContainerAdapter
+    {
+        readonly IServiceCollection _services;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NetCoreServiceCollectionContainerAdapter"/> class.
+        /// </summary>
+        /// <param name="services">The ServiceCollection.</param>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        public NetCoreServiceCollectionContainerAdapter(IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            _services = services;
+        }
+
+        /// <summary>
+        /// Resolves all handlers for the given <typeparamref name="TMessage"/> message type
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="transactionContext"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<IHandleMessages<TMessage>>> GetHandlers<TMessage>(TMessage message, ITransactionContext transactionContext)
+        {
+            var resolvedHandlerInstances = GetAllHandlersInstances<TMessage>();
+
+            transactionContext.OnDisposed(() =>
+            {
+                foreach (var disposableInstance in resolvedHandlerInstances.OfType<IDisposable>())
+                {
+                    disposableInstance.Dispose();
+                }
+            });
+
+            return resolvedHandlerInstances;
+        }
+
+        /// <summary>
+        /// Sets the bus instance that this <see cref="T:Rebus.Activation.IContainerAdapter" /> should be able to inject when resolving handler instances
+        /// </summary>
+        /// <param name="bus"></param>
+        public void SetBus(IBus bus)
+        {
+            _services.AddSingleton<IBus>(bus);
+            _services.AddTransient<IMessageContext>((s) => MessageContext.Current);
+        }
+
+        List<IHandleMessages<TMessage>> GetAllHandlersInstances<TMessage>()
+        {
+            var container = _services.BuildServiceProvider();
+
+            var handledMessageTypes = typeof(TMessage).GetBaseTypes()
+                .Concat(new[] { typeof(TMessage) });
+
+            return handledMessageTypes
+                .SelectMany(t =>
+                {
+                    var implementedInterface = typeof(IHandleMessages<>).MakeGenericType(t);
+
+                    return container.GetServices(implementedInterface).Cast<IHandleMessages>();
+                })
+                .Cast<IHandleMessages<TMessage>>()
+                .ToList();
+        }
+    }
+}
