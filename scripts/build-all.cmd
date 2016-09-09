471a26c8626e2feebc1509385ce41f7c936ae64d
Mogens Heller Grabe <mookid8000@gmail.com>
2016-09-09 08:56:40 +0200

    make build scripts

---
+++ scripts/build-all.cmd
@@ -0,0 +1,68 @@
+@echo off
+
+set version=%1
+set clean=%~dp0\clean.cmd
+set buildpackage=%~dp0\build-package.cmd
+
+REM == CHECK STUFF ==
+
+if not exist "%clean%" (
+  echo Could not find %clean%
+  exit /b 1
+)
+
+if not exist "%buildpackage%" (
+  echo Could not find %buildpackage%
+  exit /b 1
+)
+
+if not exist "%push%" (
+  echo Could not find %push%
+  exit /b 1
+)
+
+
+
+
+REM == CLEAN OUTPUT FOLDER ==
+
+call %clean%
+if %ERRORLEVEL% neq 0 (
+  echo Error calling %clean%
+  exit /b 1
+)
+
+
+
+
+REM == BUILD PACKAGES ==
+
+call %buildpackage% Rebus %1
+if %ERRORLEVEL% neq 0 (
+  echo Could not build Rebus.
+  exit /b 1
+)
+
+call %buildpackage% Rebus.Msmq %1
+if %ERRORLEVEL% neq 0 (
+  echo Could not build Rebus.Msmq.
+  exit /b 1
+)
+
+call %buildpackage% Rebus.SqlServer %1
+if %ERRORLEVEL% neq 0 (
+  echo Could not build Rebus.SqlServer.
+  exit /b 1
+)
+
+call %buildpackage% Rebus.Tests.Contracts %1
+if %ERRORLEVEL% neq 0 (
+  echo Could not build Rebus contracts.
+  exit /b 1
+)
+
+call %buildpackage% Rebus.Forklift.Common %1
+if %ERRORLEVEL% neq 0 (
+  echo Could not build Rebus common forklift things.
+  exit /b 1
+)
--- scripts/release-all.cmd
+++ scripts/release-all.cmd
@@ -2,63 +2,18 @@
 
 set version=%1
 
-set clean=%~dp0\clean.cmd
-set buildpackage=%~dp0\build-package.cmd
+set build=%~dp0\build-all.cmd
 set push=%~dp0\push.cmd
 
 
 
+REM == BUILD EVERYTHING ==
 
-REM == CHECK STUFF ==
-
-if not exist "%clean%" (
-  echo Could not find %clean%
-  exit /b 1
-)
-
-if not exist "%buildpackage%" (
-  echo Could not find %buildpackage%
-  exit /b 1
-)
-
-if not exist "%push%" (
-  echo Could not find %push%
-  exit /b 1
-)
-
-
-
-REM == CLEAN OUTPUT FOLDER ==
-
-call %clean%
+call %build% %1
 if %ERRORLEVEL% neq 0 (
-  echo Error calling %clean%
+  echo Could not build all packages.
   exit /b 1
 )
-
-
-
-
-REM == BUILD PACKAGES ==
-
-call %buildpackage% Rebus %1
-if %ERRORLEVEL% neq 0 (
-  echo Could not build Rebus.
-  exit /b 1
-)
-
-call %buildpackage% Rebus.Tests.Contracts %1
-if %ERRORLEVEL% neq 0 (
-  echo Could not build Rebus contracts.
-  exit /b 1
-)
-
-call %buildpackage% Rebus.Forklift.Common %1
-if %ERRORLEVEL% neq 0 (
-  echo Could not build Rebus common forklift things.
-  exit /b 1
-)
-
 
 
 
