eb783a2e313c0b5409a3e6b9db9f78acdcb8b868
Mogens Heller Grabe <mookid8000@gmail.com>
2017-03-17 13:15:25 +0100

    bob up the build scripts

--- scripts/release.cmd
+++ scripts/release.cmd
@@ -1,3 +1,41 @@
 @echo off
 
-call %~dp0\release-all.cmd %2
+set scriptsdir=%~dp0
+set root=%scriptsdir%\..
+set project=%1
+set version=%2
+
+if "%project%"=="" (
+	echo Please invoke the build script with a project name as its first argument.
+	echo.
+	goto exit_fail
+)
+
+if "%version%"=="" (
+	echo Please invoke the build script with a version as its second argument.
+	echo.
+	goto exit_fail
+)
+
+set Version=%version%
+
+pushd %root%
+
+dotnet pack "%project%" -c Release /p:PackageVersion=%version%
+if %ERRORLEVEL% neq 0 (
+	popd
+ 	echo Error calling %clean%
+ 	goto exit_fail
+)
+
+popd
+
+
+
+
+
+
+goto exit_success
+:exit_fail
+exit /b 1
+:exit_success
---
+++ scripts/release_old.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+call %~dp0\release-all.cmd %2
