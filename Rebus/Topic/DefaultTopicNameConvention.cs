83be711d63a75c952287dd97f7f15cc706aac8a7
Heber Ortiz Pereira <heberop@gmail.com>
2018-04-27 15:53:36 -0300

    added topic name convention extensibility

---
+++ Rebus/Topic/DefaultTopicNameConvention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rebus.Extensions;
+
+namespace Rebus.Topic
+{
+    /// <summary>
+    /// Default convention to name topics
+    /// </summary>
+    public class DefaultTopicNameConvention : ITopicNameConvention
+    {
+        /// <summary>
+        /// Returns the default topic name based on type of message
+        /// </summary>
+        public string GetTopic(Type eventType)
+        {
+            return eventType.GetSimpleAssemblyQualifiedName();
+        }
+    }
+}
