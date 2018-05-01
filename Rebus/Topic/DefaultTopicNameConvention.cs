1859366329cff50e874540a0ec45fc7d4dffb534
Mogens Heller Grabe <mookid8000@gmail.com>
2018-05-01 11:48:18 +0200

    better comment

--- Rebus/Topic/DefaultTopicNameConvention.cs
+++ Rebus/Topic/DefaultTopicNameConvention.cs
@@ -4,12 +4,14 @@ using Rebus.Extensions;
 namespace Rebus.Topic
 {
     /// <summary>
-    /// Default convention to name topics
+    /// Default convention to name topics after their "short assembly-qualified type names", which is
+    /// an assembly- and namespace-qualified type name without assembly version and public key token info.
     /// </summary>
     public class DefaultTopicNameConvention : ITopicNameConvention
     {
         /// <summary>
-        /// Returns the default topic name based on type of message
+        /// Returns the default topic name based on the "short assembly-qualified type name", which is
+        /// an assembly- and namespace-qualified type name without assembly version and public key token info.
         /// </summary>
         public string GetTopic(Type eventType)
         {
