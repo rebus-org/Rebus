b633347823fca5063dec8b6f7230a57d6ee86ed0
Mogens Heller Grabe <mookid8000@gmail.com>
2018-05-14 22:52:12 +0200

    add integration test

--- Rebus.Tests/Assumptions/TestSpecificSerializationExample.cs
+++ Rebus.Tests/Assumptions/TestSpecificSerializationExample.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
 using Rebus.Messages;
 using Rebus.Serialization.Json;
 using Rebus.Tests.Contracts;
+using Rebus.Tests.Contracts.Extensions;
 using Rebus.Tests.Extensions;
+using Rebus.Transport.InMem;
 
 namespace Rebus.Tests.Assumptions
 {
@@ -43,6 +48,50 @@ namespace Rebus.Tests.Assumptions
 {roundtrippedCommand.ToJson()}");
 
             Assert.That(roundtrippedCommand.ToJson(), Is.EqualTo(command.ToJson()));
+        }
+
+        [Test]
+        public async Task MessageLooksRightWhenReceived()
+        {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                var messageWasReceived = new ManualResetEvent(false);
+                DerivedTaskCommand receivedMessage = null;
+
+                activator.Handle<DerivedTaskCommand>(async message =>
+                {
+                    receivedMessage = message;
+                    messageWasReceived.Set();
+                });
+
+                Configure.With(activator)
+                    .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "doesn't matter"))
+                    .Start();
+
+                await activator.Bus.SendLocal(new DerivedTaskCommand
+                {
+                    Delays =
+                    {
+                        TimeSpan.FromSeconds(1),
+                        TimeSpan.FromSeconds(2),
+                        TimeSpan.FromSeconds(3),
+                        TimeSpan.FromSeconds(4),
+                        TimeSpan.FromSeconds(5),
+                    }
+                });
+
+                messageWasReceived.WaitOrDie(TimeSpan.FromSeconds(3));
+
+                Assert.That(receivedMessage, Is.Not.Null);
+                Assert.That(receivedMessage.Delays, Is.EqualTo(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(3),
+                    TimeSpan.FromSeconds(4),
+                    TimeSpan.FromSeconds(5),
+                }));
+            }
         }
 
         async Task<T> Roundtrip<T>(T obj)
