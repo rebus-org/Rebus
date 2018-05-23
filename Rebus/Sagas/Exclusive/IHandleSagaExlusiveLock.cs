360256a6a070b6af11bb906bcd08f7256f010bf1
Tor Christian <torangel@gmail.com>
2018-05-23 18:37:56 +0200

    XML comments

--- Rebus/Sagas/Exclusive/IHandleSagaExlusiveLock.cs
+++ Rebus/Sagas/Exclusive/IHandleSagaExlusiveLock.cs
@@ -2,9 +2,22 @@
 
 namespace Rebus.Sagas.Exclusive
 {
+    /// <summary>
+    /// Defines API for storing sagalocks
+    /// </summary>
     public interface IHandleSagaExlusiveLock
     {
+        /// <summary>
+        /// Aquire a lock for given key
+        /// </summary>
+        /// <param name="key">Lockingkey</param>
+        /// <returns></returns>
         Task<bool> AquireLockAsync(string key);
+        /// <summary>
+        /// Release a lock for given key
+        /// </summary>
+        /// <param name="key">Lockingkey</param>
+        /// <returns></returns>
         Task<bool> ReleaseLockAsync(string key);
 
     }
