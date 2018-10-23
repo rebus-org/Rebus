c03d53a6868f646496eca99f14ee2cd288256253
Mogens Heller Grabe <mookid8000@gmail.com>
2018-10-23 11:41:41 +0200

    move in-mem queueing of outgoing msgs to abstract transport

---
+++ Rebus/Transport/AbstractRebusTransport.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Rebus.Messages;
+
+namespace Rebus.Transport
+{
+    /// <summary>
+    /// Abstract transport implementation that implements the necessary logic to queue outgoing messages in memory,
+    /// enabling batching and whatnot if desired when sending the messages at commit time
+    /// </summary>
+    public abstract class AbstractRebusTransport : ITransport
+    {
+        protected AbstractRebusTransport(string inputQueueName)
+        {
+            Address = inputQueueName;
+        }
+
+        /// <summary>
+        /// Enqueues a
+        /// </summary>
+        /// <param name="destinationAddress"></param>
+        /// <param name="message"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Send(string destinationAddress, TransportMessage message, ITransactionContext context)
+        {
+            var outgoingMessages = context.GetOrAdd("outgoing-messages", () =>
+            {
+                var messages = new ConcurrentQueue<OutgoingMessage>();
+
+                context.OnCommitted(async () => await SendOutgoingMessages(messages, context));
+
+                return messages;
+            });
+
+            outgoingMessages.Enqueue(new OutgoingMessage(message, destinationAddress));
+        }
+
+        public abstract void CreateQueue(string address);
+
+        public abstract Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Implement this to send all outgoing messages
+        /// </summary>
+        protected abstract Task SendOutgoingMessages(IEnumerable<OutgoingMessage> outgoingMessages, ITransactionContext context);
+        
+        /// <summary>
+        /// Gets the transport's input queue address
+        /// </summary>
+        public string Address { get; }
+
+        /// <summary>
+        /// Represents one single transport messages for one particular destination
+        /// </summary>
+        public class OutgoingMessage
+        {
+            /// <summary>
+            /// Gets the transport message
+            /// </summary>
+            public TransportMessage TransportMessage { get; }
+
+            /// <summary>
+            /// Gets the destination address
+            /// </summary>
+            public string DestinationAddress { get; }
+
+            /// <summary>
+            /// Constructs the outgoing message
+            /// </summary>
+            public OutgoingMessage(TransportMessage transportMessage, string destinationAddress)
+            {
+                TransportMessage = transportMessage;
+                DestinationAddress = destinationAddress;
+            }
+        }
+    }
+}
--- Rebus/Transport/InMem/InMemTransport.cs
+++ Rebus/Transport/InMem/InMemTransport.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Rebus.Bus;
 using Rebus.Messages;
+// ReSharper disable ArgumentsStyleLiteral
 #pragma warning disable 1998
 
 namespace Rebus.Transport.InMem
@@ -12,7 +13,7 @@ namespace Rebus.Transport.InMem
     /// In-mem implementation of <see cref="ITransport"/> that uses one particular <see cref="InMemNetwork"/> to deliver messages. Can
     /// be used for in-process messaging and unit testing
     /// </summary>
-    public class InMemTransport : ITransport, ITransportInspector, IInitializable
+    public class InMemTransport : AbstractRebusTransport, ITransportInspector, IInitializable
     {
         readonly InMemNetwork _network;
         readonly string _inputQueueAddress;
@@ -22,7 +23,7 @@ namespace Rebus.Transport.InMem
         /// <paramref name="inputQueueAddress"/> as its input queue address, and thus will attempt to receive messages from the queue with that
         /// name out of the given <paramref name="network"/>
         /// </summary>
-        public InMemTransport(InMemNetwork network, string inputQueueAddress)
+        public InMemTransport(InMemNetwork network, string inputQueueAddress) : base(inputQueueAddress)
         {
             _network = network ?? throw new ArgumentNullException(nameof(network), "You need to provide a network that this in-mem transport should use for communication");
             _inputQueueAddress = inputQueueAddress;
@@ -31,32 +32,15 @@ namespace Rebus.Transport.InMem
         /// <summary>
         /// Creates a queue with the given address
         /// </summary>
-        public void CreateQueue(string address)
+        public override void CreateQueue(string address)
         {
             _network.CreateQueue(address);
         }
 
         /// <summary>
-        /// Delivers the given message to the queue identitied by the given <paramref name="destinationAddress"/>
-        /// </summary>
-        public async Task Send(string destinationAddress, TransportMessage message, ITransactionContext context)
-        {
-            if (destinationAddress == null) throw new ArgumentNullException(nameof(destinationAddress));
-            if (message == null) throw new ArgumentNullException(nameof(message));
-            if (context == null) throw new ArgumentNullException(nameof(context));
-
-            if (!_network.HasQueue(destinationAddress))
-            {
-                throw new ArgumentException($"Destination queue address '{destinationAddress}' does not exist!");
-            }
-
-            context.OnCommitted(async () => _network.Deliver(destinationAddress, message.ToInMemTransportMessage()));
-        }
-
-        /// <summary>
         /// Receives the next message from the queue identified by the configured <see cref="Address"/>, returning null if none was available
         /// </summary>
-        public async Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken)
+        public override async Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
             if (_inputQueueAddress == null) throw new InvalidOperationException("This in-mem transport is initialized without an input queue, hence it is not possible to receive anything!");
@@ -71,6 +55,14 @@ namespace Rebus.Transport.InMem
             });
 
             return nextMessage.ToTransportMessage();
+        }
+
+        protected override async Task SendOutgoingMessages(IEnumerable<OutgoingMessage> outgoingMessages, ITransactionContext context)
+        {
+            foreach (var message in outgoingMessages)
+            {
+                _network.Deliver(message.DestinationAddress, message.TransportMessage.ToInMemTransportMessage());
+            }
         }
 
         /// <summary>
