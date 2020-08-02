ca434668cd89cc316c12fd34e16ee6f84e1fabc9
Martin Humlund Clausen <martinh.c@me.com>
2020-08-02 21:46:17 +0200

    Fixed CircuitBreaker Configuration signature

--- Rebus.Tests/Retry/CircuitBreaker/CircuitBreakerTests.cs
+++ Rebus.Tests/Retry/CircuitBreaker/CircuitBreakerTests.cs
@@ -5,6 +5,7 @@ using Rebus.Retry.CircuitBreaker;
 using Rebus.Tests.Contracts;
 using Rebus.Transport.InMem;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Rebus.Tests.Retry.CircuitBreaker
@@ -12,19 +13,21 @@ namespace Rebus.Tests.Retry.CircuitBreaker
     [TestFixture]
     public class CircuitBreakerTests : FixtureBase
     {
+        // TODO: Fix this test
         [Test]
-        [Ignore("Temporary ignored, For some reason this test fails, even though worker count explicity set to 0, but worker count returns 1")]
-        public async Task Test()
+        [Ignore("Temporary ignored, For some reason this test fails, even though worker count explicity set to 0, but worker count returns 1. " +
+            "This needs to be fixed before merge")] 
+        public async Task CircuitBreakerIntegrationTest()
         {
             var network = new InMemNetwork();
 
             var receiver = Using(new BuiltinHandlerActivator());
-
             var bus = Configure.With(receiver)
+                  .Logging(l => l.Trace())
                   .Transport(t => t.UseInMemoryTransport(network, "queue-a"))
                   .Options(o =>
                       {
-                          o.SetCircuitBreakers(c => c.OpenOn<MyCustomException>(1, trackingPeriodInSeconds: 10));
+                          o.EnableCircuitBreaker(c => c.OpenOn<MyCustomException>(1, trackingPeriodInSeconds: 10));
                       }
                   )
                   .Start();
@@ -38,6 +41,7 @@ namespace Rebus.Tests.Retry.CircuitBreaker
             await bus.SendLocal("Uh oh, This is not gonna go well!");
 
             await Task.Delay(5000);
+
 
             var workerCount = bus.Advanced.Workers.Count;
             Assert.That(workerCount, Is.EqualTo(0), $"Expected worker count to be '0' but was {workerCount}");
