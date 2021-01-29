95c25b7a652bb2474919fce75cc594d5a61e3d3e
René Schultz Madsen <rm@microting.dk>
2021-01-29 07:23:59 +0000

    Trying to bump for net 5.0

--- Rebus/Transport/FileSystem/FileSystemTransport.cs
+++ Rebus/Transport/FileSystem/FileSystemTransport.cs
@@ -176,8 +176,7 @@ namespace Rebus.Transport.FileSystem
             {
                 try
                 {
-                    var fileStream = File.Open(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Delete);
-
+                    var fileStream = File.Open(file.FullName, FileMode.Open, FileAccess.ReadWrite, FileShare.Delete);
                     using (var reader = new StreamReader(fileStream, Encoding.UTF8, detectEncodingFromByteOrderMarks: false,
                         leaveOpen: true, bufferSize: 4096))
                     {
@@ -200,6 +199,7 @@ namespace Rebus.Transport.FileSystem
                 {
                     // could not acquire lock on file or it was empty
                 }
+
             }
         }
 
