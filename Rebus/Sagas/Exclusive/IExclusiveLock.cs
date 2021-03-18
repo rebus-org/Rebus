804858a024d832e7151741e8c6b9d3bbaebf790b
Kendall Bennett <kendallb@amain.com>
2021-03-18 16:01:08 -0700

    Add generic interface for locks not specific to Sagas

---
+++ Rebus/Sagas/Exclusive/IExclusiveLock.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rebus.Sagas.Exclusive
+{
+    /// <summary>
+    /// Defines API for generic exclusive locks
+    /// </summary>
+    public interface IExclusiveLock
+    {
+        /// <summary>
+        /// Aquire a lock for given key
+        /// </summary>
+        /// <param name="key">Locking key</param>
+        /// <param name="cancellationToken">Cancellation token which will be cancelled if Rebus shuts down. Can be used if e.g. distributed locks have a timeout associated with them</param>
+        /// <returns>True if the lock was aquired, false if not</returns>
+        Task<bool> AquireLockAsync(string key, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Determines if a lock has been aquired or not
+        /// </summary>
+        /// <param name="key">Locking key</param>
+        /// <param name="cancellationToken">Cancellation token which will be cancelled if Rebus shuts down. Can be used if e.g. distributed locks have a timeout associated with them</param>
+        /// <returns>True of the lock was aquired already, false if not</returns>
+        Task<bool> IsLockAquiredAsync(string key, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Release a lock for given key
+        /// </summary>
+        /// <param name="key">Locking key</param>
+        /// <returns>True of the lock was released, false if not</returns>
+        Task<bool> ReleaseLockAsync(string key);
+    }
+}
