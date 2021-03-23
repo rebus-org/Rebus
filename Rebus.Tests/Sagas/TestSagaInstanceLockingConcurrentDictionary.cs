0b4527f17e20c558f76145d1e776a3d5ee305bed
Kendall Bennett <kendallb@amain.com>
2021-03-22 18:39:47 -0700

    Exclusive Access locks for MySQL
    Fix spelling
    
    So ConcurrentDictionaryExclusiveAccessLock is the default locker implementation and you have to specifically ask to use the locker for Sagas, otherwise the default is to use the semaphore slim model. You would really only change to use the locker implementation if you set the locker up as something else, like MySQL since it is not as good as semaphores, but those are only useful for in process locking.
    Fixed comment
    Added support for registered exclusive locking interface with the configuration and build a new configuration section to support it.
    
    Updated the step handler to look for the registered handler and use that if found, otherwise fall back on the default semaphore slim model.
    
    Added support for jittered wait delays for lockers that cannot do an async wait when aquiring the lock (ConurrentDictionary and MySQL locks).
    
    Updated all unit tests and made sure the modify the customer locker to work correctly. Rebased unit tests to have a common base to test both the semaphore slim code and the concurrent dictionary code which effectively tests the API's to be used by MySQL.
    
    Added function I need for MySQL locks to check if a lock is aquired without taking the lock or blocking.

---
+++ Rebus.Tests/Sagas/TestSagaInstanceLockingConcurrentDictionary.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Rebus.Config;
+using Rebus.Sagas;
+using Rebus.Sagas.Exclusive;
+
+namespace Rebus.Tests.Sagas
+{
+    [TestFixture]
+    public class TestSagaInstanceLockingConcurrentDictionary : TestSagaInstanceLockingBase
+    {
+        protected override void EnforceExclusiveAccess(StandardConfigurer<ISagaStorage> configurer)
+        {
+            // Use the concurrent dictionary so we can test using a custom locker. Use a short sleep
+            // though for testing
+            configurer.EnforceExclusiveAccessViaLocker();
+        }
+    }
+}
---
+++ Rebus/Sagas/Exclusive/SemaphoreSlimExclusiveSagaAccessIncomingStep.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+// ReSharper disable ForCanBeConvertedToForeach
+#pragma warning disable 1998
+
+namespace Rebus.Sagas.Exclusive
+{
+    class SemaphoreSlimExclusiveSagaAccessIncomingStep : EnforceExclusiveSagaAccessIncomingStepBase, IDisposable
+    {
+        readonly SemaphoreSlim[] _locks;
+
+        public SemaphoreSlimExclusiveSagaAccessIncomingStep(int lockBuckets, CancellationToken cancellationToken)
+            : base(lockBuckets, cancellationToken)
+        {
+            _locks = Enumerable.Range(0, lockBuckets)
+                .Select(n => new SemaphoreSlim(1, 1))
+                .ToArray();
+        }
+
+        protected override async Task<bool> AcquireLockAsync(int lockId)
+        {
+            await _locks[lockId].WaitAsync(_cancellationToken);
+            return true;
+        }
+
+        protected override async Task<bool> ReleaseLockAsync(int lockId)
+        {
+            _locks[lockId].Release();
+            return true;
+        }
+
+        public override string ToString() => $"SemaphoreSlimExclusiveSagaAccessIncomingStep({_locks.Length})";
+
+        bool _disposed;
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            try
+            {
+                foreach (var disposable in _locks)
+                {
+                    disposable.Dispose();
+                }
+            }
+            finally
+            {
+                _disposed = true;
+            }
+        }
+    }
+}
