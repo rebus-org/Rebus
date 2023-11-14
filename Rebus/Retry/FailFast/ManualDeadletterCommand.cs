99d97045441aeec4a5eec3e1d2f840e1df223f05
Mogens Heller Grabe <mookid8000@gmail.com>
2023-11-14 16:39:39 +0100

    make ManualDeadletterCommand public

--- Rebus/Retry/FailFast/ManualDeadletterCommand.cs
+++ Rebus/Retry/FailFast/ManualDeadletterCommand.cs
@@ -2,11 +2,17 @@
 
 namespace Rebus.Retry.FailFast;
 
-class ManualDeadletterCommand
+/// <summary>
+/// Object that Rebus can add to its step context to signal to the retry step that it should immediately dead-letter the message being handled
+/// </summary>
+public class ManualDeadletterCommand
 {
+    /// <summary>
+    /// Gets the exception passed via this command
+    /// </summary>
     public Exception Exception { get; }
 
-    public ManualDeadletterCommand(Exception exception)
+    internal ManualDeadletterCommand(Exception exception)
     {
         Exception = exception ?? throw new ArgumentNullException(nameof(exception));
     }
