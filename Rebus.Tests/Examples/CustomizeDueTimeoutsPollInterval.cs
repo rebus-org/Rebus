55ac48d995f90b7ba03d905cac12f6a8a161d198
Mogens Heller Grabe <mookid8000@gmail.com>
2024-02-05 16:41:37 +0100

    demonstrates how to customize the due timeouts poll interval

---
+++ Rebus.Tests/Examples/CustomizeDueTimeoutsPollInterval.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Persistence.InMem;
+using Rebus.Tests.Contracts;
+using Rebus.Transport.InMem;
+
+namespace Rebus.Tests.Examples;
+
+[TestFixture]
+[Description("Just demonstrates how to configure the due timeouts poll interval")]
+public class CustomizeDueTimeoutsPollInterval : FixtureBase
+{
+    [Test]
+    public void DemonstratesHowToCustomizeTheDueTimeoutsPollInterval()
+    {
+        using var activator = new BuiltinHandlerActivator();
+
+        Configure.With(activator)
+            .Transport(t => t.UseInMemoryTransport(new(), "whatever"))
+            .Timeouts(t => t.StoreInMemory())
+            .Options(o => o.SetDueTimeoutsPollInteval(TimeSpan.FromMinutes(1)))
+            .Start();
+    }
+}
